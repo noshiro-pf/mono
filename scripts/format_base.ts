e6ae6823d52600a541ea3273a6b349b015437fd2
noshiro-pf <noshiro.pf@gmail.com>
2020-08-11 17:59:49 +0900

    update

--- scripts/format_base.ts
+++ scripts/format_base.ts
@@ -1,8 +1,9 @@
 import * as prettier from 'prettier';
 import * as tsm from 'ts-morph';
-import { cwd } from './cwd';
 
-const project = new tsm.Project({ tsConfigFilePath: `${cwd()}/tsconfig.json` });
+const project = new tsm.Project({
+  tsConfigFilePath: `${__dirname}/tsconfig.json`,
+});
 
 const getPrettierrc = async (): Promise<prettier.Options | undefined> => {
   const prettierOptions = await prettier.resolveConfig('.prettierrc');
