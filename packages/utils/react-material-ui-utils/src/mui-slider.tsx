03fc39b7d12be1806dee2c744bfd9336e9f35862
noshiro-pf <noshiro.pf@gmail.com>
2020-09-06 16:37:41 +0900

    update material-ui utils

---
+++ packages/utils/react-material-ui-utils/src/mui-slider.tsx
@@ -0,0 +1,34 @@
+import Slider from '@material-ui/core/Slider';
+import { memoNamed } from '@mono/react-utils';
+import React, { useCallback } from 'react';
+
+export const MuiSlider = memoNamed<
+  Readonly<{
+    value: number;
+    onChange: (v: number) => void;
+    ariaLabelledby: string;
+    step: number;
+    min: number;
+    max: number;
+  }>
+>('MuiSlider', ({ value, onChange, ariaLabelledby, step, min, max }) => {
+  const sliderOnChange = useCallback(
+    (_: any, value: number | number[]) => {
+      onChange(value as number);
+    },
+    [onChange]
+  );
+
+  return (
+    <Slider
+      value={value}
+      onChange={sliderOnChange}
+      aria-labelledby={ariaLabelledby}
+      valueLabelDisplay='auto'
+      marks={true}
+      step={step}
+      min={min}
+      max={max}
+    />
+  );
+});
