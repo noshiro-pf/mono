67d988f8245ab1247e915eb23709ca33b6b97db4
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2020-09-07 13:20:45 +0900

    update material-ui utils (#47)

--- packages/utils/react-material-ui-utils/src/index.ts
+++ packages/utils/react-material-ui-utils/src/index.ts
@@ -1,3 +1,5 @@
 export * from './confirm-dialog';
 export * from './img-with-loading-circle';
 export * from './img-with-loading-state';
+export * from './mui-slider';
+export * from './tabs';
---
+++ packages/utils/react-material-ui-utils/src/tabs.tsx
@@ -0,0 +1,31 @@
+import { Tab, Tabs } from '@material-ui/core';
+import { memoNamed } from '@mono/react-utils';
+import React, { useCallback } from 'react';
+
+export const MuiTabs = memoNamed<
+  Readonly<{
+    tabIndex: number;
+    tabIndexChange: (v: number) => void;
+    labels: string[];
+  }>
+>('MyTabs', ({ tabIndex, tabIndexChange, labels }) => {
+  const onChange = useCallback(
+    (_event: any, value: string) => {
+      tabIndexChange(parseInt(value, 10));
+    },
+    [tabIndexChange]
+  );
+
+  return (
+    <Tabs
+      value={tabIndex}
+      onChange={onChange}
+      indicatorColor='primary'
+      textColor='primary'
+    >
+      {labels.map((label) => (
+        <Tab key={label} label={label} />
+      ))}
+    </Tabs>
+  );
+});
