6671560259f3a47142c4d274b57f4acf14e0a093
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2020-09-07 13:20:28 +0900

    update newapp script (#44)

--- scripts/create_new_react_app.sh
+++ scripts/create_new_react_app.sh
@@ -9,13 +9,7 @@ TEMPLATE_DIR_NAME="template"
 APPS_DIR="${MONO_ROOT_DIR}/packages/apps"
 TEMPLATE_DIR="${APPS_DIR}/${TEMPLATE_DIR_NAME}"
 
-echo "APPS_DIR" ${APPS_DIR}
-echo "TEMPLATE_DIR" ${TEMPLATE_DIR}
-
 new_app_name=$1
-
-echo ${new_app_name}
-
 
 if [ -z ${new_app_name} ]; then
   echo "app name is required."
@@ -23,3 +17,4 @@ if [ -z ${new_app_name} ]; then
 fi
 
 cp -r ${TEMPLATE_DIR} "${APPS_DIR}/${new_app_name}"
+echo "created ${APPS_DIR}/${new_app_name}"
