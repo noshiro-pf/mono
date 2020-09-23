f0ab2fe7027be0b51d54b31e2a05748296f977ee
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2020-09-24 02:58:50 +0900

    update react-utils (#71)

--- packages/utils/react-utils/src/hooks/index.ts
+++ packages/utils/react-utils/src/hooks/index.ts
@@ -7,6 +7,7 @@ export * from './use-debounce';
 export * from './use-debounce-fn';
 export * from './use-previous';
 export * from './use-promise-value';
+export * from './use-state-with-mapfn';
 export * from './use-state-with-reset';
 export * from './use-toggle-state';
 export * from './use-value-as-ref';
---
+++ packages/utils/react-utils/src/hooks/use-state-with-mapfn.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from 'react';
+
+export const useStateWithMapFn = <T, S>(
+  init: T,
+  mapFn: (v: S) => T
+): [T, (value: S) => void] => {
+  const [state, setState] = useState<T>(init);
+  const setter = useCallback(
+    (value: S) => {
+      setState(mapFn(value));
+    },
+    [mapFn]
+  );
+  return [state, setter];
+};
