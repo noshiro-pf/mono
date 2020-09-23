d75bddcb442f0fcb837099d8af7392f037eeca5e
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2020-09-24 02:58:42 +0900

    update color-utils (#70)

--- packages/utils/color-utils/src/convert/hsl-to-rgb.ts
+++ packages/utils/color-utils/src/convert/hsl-to-rgb.ts
@@ -1,10 +1,41 @@
-import { TinyColor } from '@ctrl/tinycolor';
 import { Hsl, Hsla, numberToRgbValue, Rgb, Rgba } from '@mono/ts-utils';
 
+const hue2rgb = (p: number, q: number, t: number): number => {
+  if (t < 0) {
+    t += 1;
+  }
+  if (t > 1) {
+    t -= 1;
+  }
+
+  if (t < 1 / 6) return p + (q - p) * (6 * t);
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+  return p;
+};
+
 export const hslToRgb = ([h, s, l]: Hsl): Rgb => {
-  const tc = new TinyColor({ h, s, l });
-  const { r, g, b } = tc.toRgb();
-  return [numberToRgbValue(r), numberToRgbValue(g), numberToRgbValue(b)];
+  const h01 = h / 360;
+  const s01 = s / 100;
+  const l01 = l / 100;
+
+  if (s01 === 0) {
+    // achromatic
+    const l255 = numberToRgbValue(l01 * 255);
+    return [l255, l255, l255];
+  }
+
+  const q = l01 < 0.5 ? l01 * (1 + s01) : l01 + s01 - l01 * s01;
+  const p = 2 * l01 - q;
+  const r = hue2rgb(p, q, h01 + 1 / 3);
+  const g = hue2rgb(p, q, h01);
+  const b = hue2rgb(p, q, h01 - 1 / 3);
+
+  return [
+    numberToRgbValue(r * 255),
+    numberToRgbValue(g * 255),
+    numberToRgbValue(b * 255),
+  ];
 };
 
 export const hslaToRgba = ([h, s, l, a]: Hsla): Rgba => {
--- packages/utils/color-utils/src/convert/rgb-to-hsl.ts
+++ packages/utils/color-utils/src/convert/rgb-to-hsl.ts
@@ -1,4 +1,3 @@
-import { TinyColor } from '@ctrl/tinycolor';
 import {
   Hsl,
   Hsla,
@@ -9,9 +8,41 @@ import {
 } from '@mono/ts-utils';
 
 export const rgbToHsl = ([r, g, b]: Rgb): Hsl => {
-  const tc = new TinyColor({ r, g, b });
-  const { h, s, l } = tc.toHsl();
-  return [numberToHue(h), numberToPercent(100 * s), numberToPercent(100 * l)];
+  const [r01, g01, b01] = [r / 255, g / 255, b / 255];
+
+  const max = Math.max(r01, g01, b01);
+  const min = Math.min(r01, g01, b01);
+  const l = (max + min) / 2;
+
+  if (max === min) {
+    // achromatic
+    return [0, 0, numberToPercent(100 * l)];
+  }
+
+  const d = max - min;
+
+  let h = 0;
+
+  switch (max) {
+    case r01:
+      h = (g01 - b01) / d + (g01 < b01 ? 6 : 0);
+      break;
+    case g01:
+      h = (b01 - r01) / d + 2;
+      break;
+    case b01:
+      h = (r01 - g01) / d + 4;
+      break;
+    default:
+      break;
+  }
+  h /= 6;
+
+  return [
+    numberToHue(h * 360),
+    numberToPercent(100 * (l > 0.5 ? d / (2 - max - min) : d / (max + min))),
+    numberToPercent(100 * l),
+  ];
 };
 
 export const rgbaToHsla = ([r, g, b, a]: Rgba): Hsla => {
