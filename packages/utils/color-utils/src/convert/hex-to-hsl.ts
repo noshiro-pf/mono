613ee148b81d7316195ed079e4d2f4cf57fff0d5
noshiro-pf <noshiro.pf@gmail.com>
2020-09-30 01:14:15 +0900

    remove color-utils

--- packages/utils/color-utils/src/convert/hex-to-hsl.ts
+++
@@ -1,4 +0,0 @@
-import { hexToRgb, Hsl } from '@mono/ts-utils';
-import { rgbToHsl } from './rgb-to-hsl';
-
-export const hexToHsl = (hex: string): Hsl => rgbToHsl(hexToRgb(hex));
--- packages/utils/color-utils/src/convert/hex-to-hue.ts
+++
@@ -1,4 +0,0 @@
-import { Hue } from '@mono/ts-utils';
-import { hexToHsl } from './hex-to-hsl';
-
-export const hexToHue = (hex: string): Hue => hexToHsl(hex)[0];
