a7df23f3fbada193902e053422d1bcfe1172045a
noshiro-pf <noshiro.pf@gmail.com>
2021-01-24 12:18:04 +0900

    update eslintrc

---
+++ config/.eslintrc.react.ts
@@ -0,0 +1,20 @@
+import type { Linter } from 'eslint';
+
+const config: Linter.Config = {
+  root: true,
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: ['plugin:react/recommended', './.eslintrc.base.js'],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
+
+module.exports = config;
