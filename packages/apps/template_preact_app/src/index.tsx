0c016af3a55fcf10c584680cd49d1fb079b32132
noshiro-pf <noshiro.pf@gmail.com>
2021-01-24 14:15:03 +0900

    setup preact environment

---
+++ packages/apps/template_preact_app/src/index.tsx
@@ -0,0 +1,8 @@
+import { h, render } from 'preact';
+import { App } from './App';
+import './index.css';
+
+const root = document.getElementById('root');
+if (root !== null) {
+  render(<App />, root);
+}
---
+++ scripts/create_new_preact_app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# usage: yarn newapp <new-app-name>
+
+THIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
+MONO_ROOT_DIR=$(dirname ${THIS_SCRIPT_DIR})
+
+TEMPLATE_DIR_NAME="template_preact_app"
+APPS_DIR="${MONO_ROOT_DIR}/packages/apps"
+TEMPLATE_DIR="${APPS_DIR}/${TEMPLATE_DIR_NAME}"
+
+new_app_name=$1
+
+if [ -z ${new_app_name} ]; then
+  echo "app name is required."
+  exit 1
+fi
+
+cp -r ${TEMPLATE_DIR} "${APPS_DIR}/${new_app_name}"
+echo "created ${APPS_DIR}/${new_app_name}"
