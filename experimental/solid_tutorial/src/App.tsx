132c7472799bf5b5bb2e1fd11821848f95fbbb36
noshiro-pf <noshiro.pf@gmail.com>
2021-01-25 20:26:50 +0900

    remove solid_tutorial

--- experimental/solid_tutorial/src/App.tsx
+++
@@ -1,3 +0,0 @@
-import { Sub } from './components/sub';
-
-export const App = () => <Sub />;
--- experimental/solid_tutorial/src/components/sub.tsx
+++
@@ -1,9 +0,0 @@
-import { createState, onCleanup } from 'solid-js';
-
-export const Sub = () => {
-  const [state, setState] = createState<{ count: number }>({ count: 0 });
-  const timer = setInterval(() => setState({ count: state.count + 1 }), 1000);
-  onCleanup(() => clearInterval(timer));
-
-  return <div>{state.count}</div>;
-};
