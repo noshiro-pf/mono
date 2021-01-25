01a2775f66fe06fce6f84060b893cd99b0ac4eec
noshiro-pf <noshiro.pf@gmail.com>
2021-01-25 22:45:52 +0900

    create sharable lambda_calculus_interpreter_core

---
+++ packages/apps/lambda_calculus_interpreter_react/src/components/main.tsx
@@ -0,0 +1,24 @@
+import { useStateAsStream, useStreamValue } from '@mono/react-rxjs-utils';
+import { memoNamed } from '@mono/react-utils';
+import { MainView } from './main-view';
+import { useLambdaEval } from './use-lambda-eval';
+
+export const Main = memoNamed('Main', () => {
+  const [inputAreaString$, setInputAreaString] = useStateAsStream<string>(
+    '((+ 2) 3)'
+  );
+
+  const outputString$ = useLambdaEval(inputAreaString$);
+
+  /* extract values */
+  const inputAreaString = useStreamValue(inputAreaString$, '');
+  const outputAreaString = useStreamValue(outputString$, '');
+
+  return (
+    <MainView
+      inputAreaString={inputAreaString}
+      inputAreaStringChange={setInputAreaString}
+      outputAreaString={outputAreaString}
+    />
+  );
+});
--- packages/apps/lambda_calculus_interpreter_solid/src/functions/macro/number.ts
+++
@@ -1,7 +0,0 @@
-import { splitToTokens } from '../parser/split-to-tokens';
-
-const numberStr = (n: number): string =>
-  `(lambda s.(lambda z. ${'(s'.repeat(n)} z${')'.repeat(n)}))`;
-
-export const numberLambdaTerm = (n: number): string[] =>
-  splitToTokens(numberStr(n));
