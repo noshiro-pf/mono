0a6353b437930ea0274270c66eee137ce47b9b09
noshiro-pf <noshiro.pf@gmail.com>
2021-01-25 19:04:04 +0900

    add prettier plugins and remove handmade format scripts

--- scripts/format_glob.ts
+++
@@ -1,9 +0,0 @@
-import { organizeImportsAndRunPrettierWithIO } from './format_base';
-
-const globPattern = process.argv[2]; // like './src/**/*.{ts,tsx}'
-
-if (globPattern !== undefined) {
-  organizeImportsAndRunPrettierWithIO([globPattern]).catch((err) =>
-    console.error(err)
-  );
-}
