65ffe00fc335621160f0e3698e585fb318d8ec80
noshiro-pf <noshiro.pf@gmail.com>
2021-03-26 00:07:44 +0900

    create my_profile_app_preact

---
+++ packages/apps/my_profile_app_preact/config/webpack.prod.ts
@@ -0,0 +1,13 @@
+import { webpackConfigReactProdMaker } from '../../../../config/webpackconfig/react/webpack_config_prod_maker';
+import { dotenvValues } from './env';
+import { paths } from './paths';
+
+console.log('use bundle analyzer: ', dotenvValues.USE_BUNDLE_ANALYZER);
+
+const webpackConfigMerged = webpackConfigReactProdMaker(
+  paths,
+  'bundle.js',
+  dotenvValues.USE_BUNDLE_ANALYZER
+);
+
+export default webpackConfigMerged;
