43dc0f46abb814a7bea3157c0b6418c1b17e89d9
noshiro-pf <noshiro.pf@gmail.com>
2021-03-27 14:33:51 +0900

    update create_new_util.sh

--- scripts/create_new_util.sh
+++ scripts/create_new_util.sh
@@ -6,8 +6,8 @@ THIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
 MONO_ROOT_DIR=$(dirname ${THIS_SCRIPT_DIR})
 
 TEMPLATE_DIR_NAME="template"
-APPS_DIR="${MONO_ROOT_DIR}/packages/utils"
-TEMPLATE_DIR="${APPS_DIR}/${TEMPLATE_DIR_NAME}"
+UTILS_DIR="${MONO_ROOT_DIR}/packages/utils"
+TEMPLATE_DIR="${UTILS_DIR}/${TEMPLATE_DIR_NAME}"
 
 new_util_name=$1
 
@@ -16,7 +16,13 @@ if [ -z ${new_util_name} ]; then
   exit 1
 fi
 
-cp -r ${TEMPLATE_DIR} "${APPS_DIR}/${new_util_name}"
-echo "created ${APPS_DIR}/${new_util_name}"
+mkdir -p "${UTILS_DIR}/${new_util_name}"
+cp -r "${TEMPLATE_DIR}/." "${UTILS_DIR}/${new_util_name}/"
+
+new_util_name_kebab=$(echo "${new_util_name}" | sed "s/_/-/g")
+echo ${new_util_name_kebab}
+sed -i "s/template-utils/${new_util_name_kebab}/" "${UTILS_DIR}/${new_util_name}/package.json"
+
+echo "created ${UTILS_DIR}/${new_util_name}"
 
 echo "don't forget to add \"${new_util_name}\" to \"build_utils.sh\" targets!"
