0036632cbc40b82341e3fe08d220080ed9cf1123
noshiro-pf <noshiro.pf@gmail.com>
2021-04-20 15:21:24 +0900

    create preact-resize-observer-hooks

---
+++ packages/utils/preact-resize-observer-hooks/src/index.ts
@@ -0,0 +1,52 @@
+import { isArrayOfLength1OrMore, RectSize } from '@noshiro/ts-utils';
+import { RefObject } from 'preact';
+import { useEffect, useMemo, useRef, useState } from 'preact/compat';
+import { ResizeObserver } from 'resize-observer';
+
+export const useResizeObserverRef = (
+  setRootRectSize: (v: RectSize) => void
+): RefObject<HTMLDivElement> => {
+  const rootResizeObserver = useMemo(
+    () =>
+      new ResizeObserver((entries) => {
+        if (isArrayOfLength1OrMore(entries)) {
+          const contentRect = entries[0].contentRect;
+          setRootRectSize({
+            width: contentRect.width,
+            height: contentRect.height,
+          });
+        }
+      }),
+    [setRootRectSize]
+  );
+
+  const targetElRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const el = targetElRef.current;
+    if (el !== null) {
+      rootResizeObserver.observe(el);
+    }
+    return () => {
+      if (el !== null) {
+        rootResizeObserver.unobserve(el);
+      }
+      rootResizeObserver.disconnect();
+    };
+  }, [targetElRef, rootResizeObserver]);
+
+  return targetElRef;
+};
+
+export const useResizeObserver = (
+  defaultRectSize: RectSize = {
+    width: 0,
+    height: 0,
+  }
+): [RectSize, RefObject<HTMLDivElement>] => {
+  const [rootRectSize, setRootRectSize] = useState<RectSize>(defaultRectSize);
+
+  const targetElRef = useResizeObserverRef(setRootRectSize);
+
+  return [rootRectSize, targetElRef];
+};
