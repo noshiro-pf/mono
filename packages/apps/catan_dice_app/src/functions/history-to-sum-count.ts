c81879cae701d562beceb0b2b13b35d6939c55bb
noshiro-pf <noshiro.pf@gmail.com>
2021-05-01 00:01:03 +0900

    refactor catan_dice_app

--- packages/apps/catan_dice_app/src/functions/history-to-sum-count.ts
+++ packages/apps/catan_dice_app/src/functions/history-to-sum-count.ts
@@ -1,9 +1,10 @@
+import { uint32, zeros } from '@noshiro/ts-utils';
 import { IList } from '../immutable';
 import { THistoryState } from '../type/history';
 
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 export const historyToSumCount = (history: THistoryState): IList<number> => {
-  const count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const count = zeros(11 as uint32);
   const historyFiltered = history.history.take(history.index + 1);
   historyFiltered.forEach(([a, b]) => {
     count[a + b - 2] += 1;
