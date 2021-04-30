26aa9cb1033aa82f31c33910bea054ca4b61ed05
noshiro-pf <noshiro.pf@gmail.com>
2021-04-30 16:54:32 +0900

    fix "@typescript-eslint/prefer-readonly-parameter-types" warnings in catan_dice_app

--- packages/apps/catan_dice_app/src/type/history.ts
+++ packages/apps/catan_dice_app/src/type/history.ts
@@ -2,12 +2,12 @@ import { IList, IRecord } from '../immutable';
 
 type IHistoryState = Readonly<{
   index: number;
-  history: IList<[number, number]>;
+  history: IList<readonly [number, number]>;
 }>;
 
 export const HistoryState = IRecord<IHistoryState>({
   index: -1,
-  history: IList<[number, number]>([]),
-});
+  history: IList<readonly [number, number]>([]),
+} as const);
 
 export type THistoryState = IRecord<IHistoryState> & Readonly<IHistoryState>;
