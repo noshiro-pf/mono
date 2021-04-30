06d04294ad7c55a0f8ebb4f5b6e785cc833aa175
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2021-04-30 17:34:13 +0900

    Add workspace npm command (#217)
    * add lint:apps, lint:utils command
    
    * update apps.txt (sort alphabetically & add template apps)

---
+++ scripts/lint_utils.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+targets=(
+  # no local dependency
+  "ts-utils"
+
+  # ts-utils dependent
+  "syncflow"
+  "react-syncflow-hooks"
+  "preact-syncflow-hooks"
+  "rxjs-utils"
+  "react-router-utils"
+  "react-utils"
+  "preact-utils"
+  "preact-resize-observer-hooks"
+  "react-resize-observer-hooks"
+  
+  # dependencies: [react-utils, rxjs-utils, ts-utils]
+  "react-rxjs-utils"
+  "preact-rxjs-utils"
+
+  # dependencies: [react-utils, react-resize-observer-hooks, ts-utils]
+  "react-utils-styled"
+
+  # dependencies: [react-utils, react-utils-styled, react-resize-observer-hooks, ts-utils]
+  "react-material-ui-utils"
+  "react-blueprintjs-utils"
+)
+
+THIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
+MONO_ROOT_DIR=$(dirname ${THIS_SCRIPT_DIR})
+utils_path="${MONO_ROOT_DIR}/packages/utils"
+echo ${utils_path}
+
+for target in "${targets[@]}" ; do
+    echo "linting \"${target}\" ..." 
+    cd "${utils_path}/${target}"
+    yarn lint
+    echo "done."
+    echo ""
+done
+
+echo "linting \"lambda_calculus_interpreter_core\" ..." 
+apps_path="${MONO_ROOT_DIR}/packages/apps"
+cd "${apps_path}/lambda_calculus_interpreter_core"
+yarn lint
+echo "done."
