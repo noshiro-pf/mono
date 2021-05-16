33f6cfbce20220f5e45e380af859555f3c57676a
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2021-05-17 08:04:58 +0900

    create hanabi_history_app (#237)

---
+++ packages/apps/hanabi_history_app/config/paths.ts
@@ -0,0 +1,11 @@
+import { Paths } from '../../../../config/webpackconfig/react/paths_type';
+import { resolveAppPath } from './app_directory';
+import { dotenvValues } from './env';
+
+export const paths: Paths = {
+  appBuild: resolveAppPath('build'),
+  appIndexJs: resolveAppPath('src/index.tsx'),
+  publicUrlOrPath: dotenvValues.PUBLIC_URL ?? '/',
+  contentBase: resolveAppPath('public', true),
+  tsconfigJson: resolveAppPath('tsconfig.json'),
+};
