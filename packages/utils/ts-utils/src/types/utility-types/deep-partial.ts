0672afc377014ba67fe71a4f9bcf17cbf49981d1
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2021-05-21 11:33:11 +0900

    fix utility-types of DeepReadonly, DeepWritable and DeepPartial (#251)

--- packages/utils/ts-utils/src/types/utility-types/deep-partial.ts
+++ packages/utils/ts-utils/src/types/utility-types/deep-partial.ts
@@ -1,7 +1,23 @@
-export type DeepPartial<S> = {
-  [K in keyof S]?: S[K] extends (infer U)[]
-    ? DeepPartial<U>[]
-    : S[K] extends Record<K, S[K]>
-    ? DeepPartial<S[K]>
-    : S[K];
-};
+import type { TypeEq } from './test-type';
+import { assertType } from './test-type';
+
+export type DeepPartial<T> = T extends (infer R)[]
+  ? DeepPartial<R>[]
+  : T extends (...args: readonly unknown[]) => unknown
+  ? T
+  : T extends Record<string, unknown>
+  ? { [P in keyof T]?: DeepPartial<T[P]> }
+  : T;
+
+assertType<
+  TypeEq<
+    DeepPartial<{ a: { b: { c: [1, 2, 3] } } }>,
+    {
+      a?: {
+        b?: {
+          c?: (1 | 2 | 3)[];
+        };
+      };
+    }
+  >
+>();
