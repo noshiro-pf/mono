20c1d12d3043e376b8613063f49cf2d1ed542f43
noshiro-pf <noshiro.pf@gmail.com>
2021-07-21 05:43:34 +0900

    update eslintrc for react

--- config/eslintrc/eslintrc-base/index.ts
+++ config/eslintrc/eslintrc-base/index.ts
@@ -1,4 +1,5 @@
 export * from './eslint-import-rules';
 export * from './eslint-noshiro-custom';
+export * from './eslint-react-rules';
 export * from './eslint-rules';
 export * from './typescript-eslint-rules';
