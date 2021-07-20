d179e19aad8100be3e25b46a8d011c6e3fcd491b
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2021-07-21 02:19:56 +0900

    update publish.sh (#284)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,12 +3,24 @@
 THIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
 MONO_ROOT_DIR=$(dirname ${THIS_SCRIPT_DIR})
 
+function buildPkg () {
+  cd "${MONO_ROOT_DIR}/$1"
+  yarn build
+}
+
 function publishPkg () {
   yarn publish "${MONO_ROOT_DIR}/$1" --patch --no-git-tag-version --access=public
 }
 
-publishPkg packages/utils/ts-utils
-publishPkg packages/utils/syncflow
-publishPkg packages/utils/preact-syncflow-hooks
-publishPkg packages/utils/react-syncflow-hooks
-publishPkg packages/apps/event_schedule_app_shared
+function buildAndPublishPkg () {
+  buildPkg $1;
+  publishPkg $1
+}
+
+buildAndPublishPkg packages/utils/ts-utils
+buildAndPublishPkg packages/utils/syncflow
+buildAndPublishPkg packages/utils/preact-syncflow-hooks
+buildAndPublishPkg packages/utils/react-syncflow-hooks
+buildAndPublishPkg packages/apps/event_schedule_app_shared
+buildAndPublishPkg packages/utils/ts-utils
+buildAndPublishPkg packages/tools/eslint-custom-rules
