4389dec7166153145b1f2864f7ebc7b6fca89a3c
noshiro-pf <noshiro.pf@gmail.com>
2021-07-21 05:13:11 +0900

    use typescript-eslint all

--- config/eslintrc/eslintrc-base/typescript-eslint-rules.ts
+++ config/eslintrc/eslintrc-base/typescript-eslint-rules.ts
@@ -1,21 +1,20 @@
 import type { Linter } from 'eslint';
 
 export const typescriptEslintRules: Readonly<
-  Record<
-    | 'additionalRulesNotIncludedInRecommended'
-    | 'disabledRulesIncludedInRecommended'
-    | 'modifiedRulesIncludedInRecommended',
-    Partial<Linter.RulesRecord>
-  >
+  Record<'disabledRules' | 'modifiedRules', Partial<Linter.RulesRecord>>
 > = {
-  additionalRulesNotIncludedInRecommended: {
-    '@typescript-eslint/array-type': 'error',
-    '@typescript-eslint/ban-tslint-comment': 'error',
-    // '@typescript-eslint/class-literal-property-style': 'error',
-    '@typescript-eslint/consistent-indexed-object-style': 'error',
-    '@typescript-eslint/consistent-type-assertions': 'error',
+  modifiedRules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          Omit: 'Use `StrictOmit` instead.',
+          Exclude: 'Use `StrictExclude` instead.',
+        },
+        extendDefaults: true,
+      },
+    ],
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/explicit-function-return-type': [
       'warn',
       {
@@ -28,63 +27,6 @@ export const typescriptEslintRules: Readonly<
       'error',
       { accessibility: 'no-public' },
     ],
-    // '@typescript-eslint/member-delimiter-style': 'error',  -> scope of prettier
-    // '@typescript-eslint/member-ordering': 'error', -> off because I use class only occasionally
-    '@typescript-eslint/method-signature-style': 'error',
-    // '@typescript-eslint/naming-convention': 'error',
-    '@typescript-eslint/no-base-to-string': 'error',
-    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
-    '@typescript-eslint/no-confusing-void-expression': 'error',
-    '@typescript-eslint/no-dynamic-delete': 'error',
-    '@typescript-eslint/no-extraneous-class': 'error',
-    '@typescript-eslint/no-implicit-any-catch': 'error',
-    '@typescript-eslint/no-invalid-void-type': 'error',
-    '@typescript-eslint/no-parameter-properties': 'error',
-    '@typescript-eslint/no-require-imports': 'error',
-    // '@typescript-eslint/no-type-alias': 'error',
-    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-    // '@typescript-eslint/no-unnecessary-condition': 'error',  -> unnecessary in favor of strict-boolean-expressions
-    '@typescript-eslint/no-unnecessary-qualifier': 'error', // maybe unnecessary
-    '@typescript-eslint/no-unnecessary-type-arguments': 'error',
-    '@typescript-eslint/no-unnecessary-type-constraint': 'error',
-    '@typescript-eslint/no-unsafe-return': 'error',
-    '@typescript-eslint/non-nullable-type-assertion-style': 'error',
-    // '@typescript-eslint/prefer-enum-initializers': 'error',  -> unnecessary because I don't use enum.
-    '@typescript-eslint/prefer-for-of': 'error',
-    '@typescript-eslint/prefer-function-type': 'error',
-    '@typescript-eslint/prefer-includes': 'error',
-    // '@typescript-eslint/prefer-literal-enum-member': 'error',  -> unnecessary because I don't use enum.
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
-    '@typescript-eslint/prefer-optional-chain': 'error',
-    '@typescript-eslint/prefer-readonly': 'error',
-    '@typescript-eslint/prefer-readonly-parameter-types': 'off', // -> use eslint-plugin-noshiro-custom/prefer-readonly-parameter-types
-    '@typescript-eslint/prefer-reduce-type-parameter': 'error',
-    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-    '@typescript-eslint/prefer-ts-expect-error': 'error',
-    // '@typescript-eslint/promise-function-async': 'error',
-    '@typescript-eslint/require-array-sort-compare': 'error', // important
-    '@typescript-eslint/sort-type-union-intersection-members': 'error',
-    '@typescript-eslint/strict-boolean-expressions': [
-      'error',
-      {
-        allowString: false,
-        allowNumber: false,
-        allowNullableObject: false,
-      },
-    ], // important
-    '@typescript-eslint/switch-exhaustiveness-check': 'error',
-    // '@typescript-eslint/type-annotation-spacing': 'error',  -> scope of prettier
-    // '@typescript-eslint/typedef': 'error',  -> unnecessary if noImplicitAny and/or strictPropertyInitialization is enabled.
-    '@typescript-eslint/unified-signatures': 'error',
-
-    // extension rules
-    'dot-notation': 'off',
-    '@typescript-eslint/dot-notation': 'error',
-    'no-dupe-class-members': 'off',
-    '@typescript-eslint/no-dupe-class-members': 'error',
-    'no-unused-expressions': 'off',
-    '@typescript-eslint/no-unused-expressions': 'error',
-    'no-shadow': 'off',
     '@typescript-eslint/no-shadow': [
       'warn',
       {
@@ -95,18 +37,7 @@ export const typescriptEslintRules: Readonly<
         ignoreFunctionTypeParameterNameValueShadow: false,
       },
     ],
-  },
-  modifiedRulesIncludedInRecommended: {
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          Omit: 'Use `StrictOmit` instead.',
-          Exclude: 'Use `StrictExclude` instead.',
-        },
-        extendDefaults: true,
-      },
-    ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/restrict-plus-operands': [
       'error',
       { checkCompoundAssignments: true },
@@ -115,16 +46,26 @@ export const typescriptEslintRules: Readonly<
       'error',
       { allowNumber: true, allowBoolean: true, allowNullable: true },
     ],
+    '@typescript-eslint/strict-boolean-expressions': [
+      'error',
+      { allowString: false, allowNumber: false, allowNullableObject: false },
+    ],
+    '@typescript-eslint/no-unnecessary-condition': [
+      'error',
+      { allowConstantLoopConditions: true },
+    ],
   },
-  disabledRulesIncludedInRecommended: {
-    'no-unused-vars': ['off'],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
+  disabledRules: {
+    '@typescript-eslint/lines-between-class-members': 'off',
+    '@typescript-eslint/member-ordering': 'off',
+    '@typescript-eslint/naming-convention': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-magic-numbers': 'off',
     '@typescript-eslint/no-namespace': 'off',
-    // 'no-explicit-any': 'off',
-    // '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+    '@typescript-eslint/no-type-alias': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/promise-function-async': 'off',
   },
 };
