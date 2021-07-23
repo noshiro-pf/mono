09b7a183b75333d1202f47d34a4c3c2f0d1aabbd
noshiro-pf <noshiro.pf@gmail.com>
2021-07-23 14:32:14 +0900

    update eslintrc react

--- config/eslintrc/.eslintrc.react.ts
+++ config/eslintrc/.eslintrc.react.ts
@@ -16,8 +16,22 @@ const config: Linter.Config = {
   rules: {
     //   'react-hooks/rules-of-hooks': 'error',
     //   'react-hooks/exhaustive-deps': 'warn',
+
+    /* disabled */
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/jsx-handler-names': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    'react/jsx-max-depth': 'off',
+    'react/jsx-boolean-value': 'off',
+
+    /* modified */
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'react/forbid-component-props': ['error', { forbid: ['className'] }],
+    'react/no-array-index-key': 'warn',
+
+    // TODO
+    'react/jsx-sort-props': 'warn',
   },
 };
 
