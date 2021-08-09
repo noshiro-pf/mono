7bf4ec29141039d62c0ba7bfb6352b841dafa9b9
noshiro-pf <noshiro.pf@gmail.com>
2021-08-10 05:36:35 +0900

    feat: implement goober with typescript [WIP]

---
+++ experimental/packages/utils/goober/src/core/parse.ts
@@ -0,0 +1,67 @@
+/**
+ * Parses the object into css, scoped, blocks
+ */
+export const parse = (obj: object, selector: string): string => {
+  let outer = '';
+  let blocks = '';
+  let current = '';
+  let next;
+
+  for (const key in obj) {
+    const val = obj[key];
+
+    // If this is a 'block'
+    if (typeof val === 'object') {
+      next = selector
+        ? // Go over the selector and replace the matching multiple selectors if any
+          selector.replace(/([^,])+/g, (sel) =>
+            // Return the current selector with the key matching multiple selectors if any
+            key.replace(/([^,])+/g, (k) => {
+              // If the current `k`(key) has a nested selector replace it
+              if (/&/g.test(k)) return k.replace(/&/g, sel);
+
+              // If there's a current selector concat it
+              return sel ? `${sel} ${k}` : k;
+            })
+          )
+        : key;
+
+      // If these are the `@` rule
+      if (key.startsWith('@')) {
+        // Handling the `@font-face` where the
+        // block doesn't need the brackets wrapped
+        if (key[1] == 'f') {
+          blocks += parse(val, key);
+        } else {
+          // Regular rule block
+          blocks += `${key}{${parse(val, key[1] == 'k' ? '' : selector)}}`;
+        }
+      } else {
+        // Call the parse for this block
+        blocks += parse(val, next);
+      }
+    } else {
+      if (key.startsWith('@') && key[1] == 'i') {
+        outer = `${key} ${val};`;
+      } else {
+        // Push the line for this property
+        current += parse.p
+          ? // We have a prefixer and we need to run this through that
+            parse.p(key.replace(/[A-Z]/g, '-$&').toLowerCase(), val)
+          : // Nope no prefixer just append it
+            `${key.replace(/[A-Z]/g, '-$&').toLowerCase()}:${val};`;
+      }
+    }
+  }
+
+  // If we have properties
+  if (current[0]) {
+    // Standard rule composition
+    next = selector ? `${selector}{${current}}` : current;
+
+    // Else just push the rule
+    return outer + next + blocks;
+  }
+
+  return outer + blocks;
+};
