966d5000d239a62bf8e0cd3e524eb7d5fd204911
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2021-09-24 15:04:35 +0900

    update ts-utils v1.1.2 (#338)

--- packages/utils/ts-utils/src/immutable/index.ts
+++ packages/utils/ts-utils/src/immutable/index.ts
@@ -2,6 +2,5 @@ export * from './imap';
 export * from './imap-mapped';
 export * from './iset';
 export * from './iset-mapped';
-export * from './key-base-type';
 export * from './list';
 export * from './record';
