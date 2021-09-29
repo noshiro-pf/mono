e55f2fbc1f888496de764744ba9e2020cd8b27da
noshiro-pf <noshiro.pf@gmail.com>
2021-09-30 02:19:34 +0900

    update my_profile_app_preact

--- packages/apps/my_profile_app_preact/src/components/mui/index.ts
+++ packages/apps/my_profile_app_preact/src/components/mui/index.ts
@@ -1,3 +1,4 @@
+export * from './icons';
 export * from './mui-app-bar';
 export * from './mui-card';
 export * from './mui-card-content';
--- packages/apps/my_profile_app_preact/src/constants/index.ts
+++ packages/apps/my_profile_app_preact/src/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './media-query';
 export * from './routes';
+export * from './url';
