e133d167527316bd9653fb59ef53bd5426f07dfc
noshiro-pf <noshiro.pf@gmail.com>
2021-10-02 05:00:52 +0900

    update firebase versions to v9

--- packages/apps/event_schedule_app/src/api/crud/delete-answer.ts
+++ packages/apps/event_schedule_app/src/api/crud/delete-answer.ts
@@ -1,14 +1,11 @@
 import type { Answer } from '@noshiro/event-schedule-app-shared';
 import { firestorePaths } from '@noshiro/event-schedule-app-shared';
+import { deleteDoc, doc } from 'firebase/firestore';
 import { dbEvents } from '../../initialize-firebase';
 
 export const deleteAnswer = async (
   eventId: string,
   answerId: Answer['id']
 ): Promise<void> => {
-  await dbEvents
-    .doc(eventId)
-    .collection(firestorePaths.answers)
-    .doc(answerId)
-    .delete();
+  await deleteDoc(doc(dbEvents, eventId, firestorePaths.answers, answerId));
 };
--- packages/apps/event_schedule_app/src/api/crud/get-event-schedule.ts
+++ packages/apps/event_schedule_app/src/api/crud/get-event-schedule.ts
@@ -1,14 +1,15 @@
 import type { EventSchedule } from '@noshiro/event-schedule-app-shared';
 import { fillEventSchedule } from '@noshiro/event-schedule-app-shared';
 import { Result } from '@noshiro/ts-utils';
+import { doc, getDoc } from 'firebase/firestore';
 import { dbEvents } from '../../initialize-firebase';
 
 export const getEventSchedule = async (
   id: string
 ): Promise<Result<EventSchedule, 'not-found' | 'others'>> => {
   try {
-    const res = await dbEvents.doc(id).get();
-    if (!res.exists) {
+    const res = await getDoc(doc(dbEvents, id));
+    if (!res.exists()) {
       console.log(`event of id "${id}" not-found`);
       return Result.err('not-found');
     }
