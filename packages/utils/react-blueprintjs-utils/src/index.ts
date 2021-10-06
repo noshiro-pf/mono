0b7a0e7f57a6073502e6f823150e126a119b8786
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2021-10-07 04:35:21 +0900

    add reference to stdlib in utils (#362)

--- packages/utils/react-blueprintjs-utils/src/index.ts
+++ packages/utils/react-blueprintjs-utils/src/index.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+
+/// <reference no-default-lib="true"/>
+/// <reference path="../../stdlib/dist/lib.dom.d.ts" />
+/// <reference path="../../stdlib/dist/lib.esnext.d.ts" />
+/// <reference path="../../stdlib/dist/utils.d.ts" />
+
 export * from './bp-button';
 export * from './bp-checkbox';
 export * from './bp-date-picker';
