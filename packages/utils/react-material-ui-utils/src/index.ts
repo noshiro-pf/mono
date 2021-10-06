d2ab9fe3c4decfe6098dac378cb4c9a603c5f8e8
noshiro-pf <noshiro.pf@gmail.com>
2021-10-07 04:03:54 +0900

    add reference to stdlib in utils

--- packages/utils/react-material-ui-utils/src/index.ts
+++ packages/utils/react-material-ui-utils/src/index.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+
+/// <reference no-default-lib="true"/>
+/// <reference path="../../stdlib/dist/lib.dom.d.ts" />
+/// <reference path="../../stdlib/dist/lib.esnext.d.ts" />
+/// <reference path="../../stdlib/dist/utils.d.ts" />
+
 export * from './confirm-dialog';
 export * from './img-with-loading-circle';
 export * from './img-with-loading-state';
