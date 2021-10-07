c88a629a6eaf25c4a311a2b42eb88f5200cecc9e
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2021-10-07 12:28:23 +0900

    update syncflow hooks (#366)

---
+++ packages/utils/syncflow-preact-hooks/src/use-event-stream.ts
@@ -0,0 +1,23 @@
+import type { Observable } from '@noshiro/syncflow';
+import { source } from '@noshiro/syncflow';
+import { useCallback, useMemo } from 'preact/compat';
+
+export const useVoidEventAsStream = (): [Observable<void>, () => void] => {
+  const src$ = useMemo(() => source<undefined>(), []);
+
+  const emitter = useCallback(() => {
+    src$.next(undefined);
+  }, []);
+
+  return [src$, emitter];
+};
+
+export const useEventAsStream = <A>(): [Observable<A>, (value: A) => void] => {
+  const src$ = useMemo(() => source<A>(), []);
+
+  const emitter = useCallback((value: A) => {
+    src$.next(value);
+  }, []);
+
+  return [src$, emitter];
+};
--- packages/utils/syncflow-react-hooks/src/index.ts
+++ packages/utils/syncflow-react-hooks/src/index.ts
@@ -5,4 +5,8 @@
 /// <reference path="../../stdlib/dist/lib.esnext.d.ts" />
 /// <reference path="../../stdlib/dist/utils.d.ts" />
 
-export * from './hooks';
+export * from './use-event-stream';
+export * from './use-reducer-as-stream';
+export * from './use-state-as-stream';
+export * from './use-stream';
+export * from './use-value-as-stream';
