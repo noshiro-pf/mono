f3ae5eb19c1a1dfe53d968ca7daad16bf0b87096
noshiro-pf <noshiro.pf@gmail.com>
2022-03-13 17:43:16 +0900

    [dev] custom eslint wip

--- packages/tools/eslint-custom-rules/src/rules/prefer-readonly-parameter-types.ts
+++ packages/tools/eslint-custom-rules/src/rules/prefer-readonly-parameter-types.ts
@@ -1,152 +1,112 @@
-import { ESLintUtils } from '@typescript-eslint/experimental-utils';
-import type { TSESTree } from '@typescript-eslint/types';
-import { AST_NODE_TYPES } from '@typescript-eslint/types';
-import type { __String } from 'typescript';
-import { isTypeReadonly } from '../utils/isTypeReadonly';
+import { AST_NODE_TYPES, type TSESTree } from '@typescript-eslint/utils';
+import * as util from '../util';
 
-type Options = readonly [
-  Readonly<{
+type Options = [
+  util.ReadonlynessOptions & {
     checkParameterProperties?: boolean;
     ignoreInferredTypes?: boolean;
-    allow?: readonly __String[];
-  }>,
+  },
 ];
 
 type MessageIds = 'shouldBeReadonly';
 
-const createRule = ESLintUtils.RuleCreator(
-  () =>
-    `https://github.com/typescript-eslint/typescript-eslint/blob/v4.24.0/packages/eslint-plugin/docs/rules/prefer-readonly-parameter-types.md`,
-);
-
-export const preferReadonlyParameterTypesRuleName =
-  'prefer-readonly-parameter-types';
-
-export const preferReadonlyParameterTypesRule = createRule<Options, MessageIds>(
-  {
-    name: preferReadonlyParameterTypesRuleName,
-    meta: {
-      type: 'suggestion',
-      docs: {
-        description:
-          'Requires that function parameters are typed as readonly to prevent accidental mutation of inputs',
-        category: 'Possible Errors',
-        recommended: false,
-        requiresTypeChecking: true,
-      },
-      schema: [
-        {
-          type: 'object',
-          additionalProperties: false,
-          properties: {
-            checkParameterProperties: {
-              type: 'boolean',
-            },
-            ignoreInferredTypes: {
-              type: 'boolean',
-            },
-            allow: {
-              type: 'array',
-              items: {
-                type: 'string',
-              },
-            },
-          },
-        },
-      ],
-      messages: {
-        shouldBeReadonly: 'Parameter should be a read only type.',
-      },
+export default util.createRule<Options, MessageIds>({
+  name: 'prefer-readonly-parameter-types',
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description:
+        'Requires that function parameters are typed as readonly to prevent accidental mutation of inputs',
+      recommended: false,
+      requiresTypeChecking: true,
     },
-    defaultOptions: [
+    schema: [
       {
-        checkParameterProperties: true,
-        ignoreInferredTypes: false,
-        allow: [],
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          checkParameterProperties: {
+            type: 'boolean',
+          },
+          ignoreInferredTypes: {
+            type: 'boolean',
+          },
+          ...util.readonlynessOptionsSchema.properties,
+        },
       },
     ],
-    create(context, options) {
-      const [{ checkParameterProperties, ignoreInferredTypes, allow }] =
-        options;
-      const { esTreeNodeToTSNodeMap, program } =
-        ESLintUtils.getParserServices(context);
-      const checker = program.getTypeChecker();
+    messages: {
+      shouldBeReadonly: 'Parameter should be a read only type.',
+    },
+  },
+  defaultOptions: [
+    {
+      checkParameterProperties: true,
+      ignoreInferredTypes: false,
+      ...util.readonlynessOptionsDefaults,
+    },
+  ],
+  create(
+    context,
+    [{ checkParameterProperties, ignoreInferredTypes, treatMethodsAsReadonly }],
+  ) {
+    const { esTreeNodeToTSNodeMap, program } = util.getParserServices(context);
+    const checker = program.getTypeChecker();
 
-      return {
-        [[
-          AST_NODE_TYPES.ArrowFunctionExpression,
-          AST_NODE_TYPES.FunctionDeclaration,
-          AST_NODE_TYPES.FunctionExpression,
-          AST_NODE_TYPES.TSCallSignatureDeclaration,
-          AST_NODE_TYPES.TSConstructSignatureDeclaration,
-          AST_NODE_TYPES.TSDeclareFunction,
-          AST_NODE_TYPES.TSEmptyBodyFunctionExpression,
-          AST_NODE_TYPES.TSFunctionType,
-          AST_NODE_TYPES.TSMethodSignature,
-        ].join(', ')](
-          node:
-            | TSESTree.ArrowFunctionExpression
-            | TSESTree.FunctionDeclaration
-            | TSESTree.FunctionExpression
-            | TSESTree.TSCallSignatureDeclaration
-            | TSESTree.TSConstructSignatureDeclaration
-            | TSESTree.TSDeclareFunction
-            | TSESTree.TSEmptyBodyFunctionExpression
-            | TSESTree.TSFunctionType
-            | TSESTree.TSMethodSignature,
-        ): void {
-          for (const param of node.params) {
-            if (
-              checkParameterProperties === false &&
-              param.type === AST_NODE_TYPES.TSParameterProperty
-            ) {
-              continue;
-            }
+    return {
+      [[
+        AST_NODE_TYPES.ArrowFunctionExpression,
+        AST_NODE_TYPES.FunctionDeclaration,
+        AST_NODE_TYPES.FunctionExpression,
+        AST_NODE_TYPES.TSCallSignatureDeclaration,
+        AST_NODE_TYPES.TSConstructSignatureDeclaration,
+        AST_NODE_TYPES.TSDeclareFunction,
+        AST_NODE_TYPES.TSEmptyBodyFunctionExpression,
+        AST_NODE_TYPES.TSFunctionType,
+        AST_NODE_TYPES.TSMethodSignature,
+      ].join(', ')](
+        node:
+          | TSESTree.ArrowFunctionExpression
+          | TSESTree.FunctionDeclaration
+          | TSESTree.FunctionExpression
+          | TSESTree.TSCallSignatureDeclaration
+          | TSESTree.TSConstructSignatureDeclaration
+          | TSESTree.TSDeclareFunction
+          | TSESTree.TSEmptyBodyFunctionExpression
+          | TSESTree.TSFunctionType
+          | TSESTree.TSMethodSignature,
+      ): void {
+        for (const param of node.params) {
+          if (
+            !checkParameterProperties &&
+            param.type === AST_NODE_TYPES.TSParameterProperty
+          ) {
+            continue;
+          }
 
-            const actualParam =
-              param.type === AST_NODE_TYPES.TSParameterProperty
-                ? param.parameter
-                : param;
+          const actualParam =
+            param.type === AST_NODE_TYPES.TSParameterProperty
+              ? param.parameter
+              : param;
 
-            if (
-              ignoreInferredTypes === true &&
-              actualParam.typeAnnotation == null
-            ) {
-              continue;
-            }
+          if (ignoreInferredTypes && actualParam.typeAnnotation == null) {
+            continue;
+          }
 
-            const tsNode = esTreeNodeToTSNodeMap.get(actualParam);
-            const type = checker.getTypeAtLocation(tsNode);
-            const isReadOnly = isTypeReadonly(checker, type);
+          const tsNode = esTreeNodeToTSNodeMap.get(actualParam);
+          const type = checker.getTypeAtLocation(tsNode);
+          const isReadOnly = util.isTypeReadonly(checker, type, {
+            treatMethodsAsReadonly: treatMethodsAsReadonly!,
+          });
 
-            if (isReadOnly) {
-              continue;
-            }
-
-            if (allow !== undefined) {
-              if (
-                type.symbol !== undefined &&
-                allow.includes(type.symbol.escapedName)
-              ) {
-                continue;
-              }
-              if (
-                type.aliasSymbol !== undefined &&
-                allow.includes(type.aliasSymbol.escapedName)
-              ) {
-                continue;
-              }
-            }
-
+          if (!isReadOnly) {
             context.report({
               node: actualParam,
               messageId: 'shouldBeReadonly',
             });
           }
-        },
-      };
-    },
+        }
+      },
+    };
   },
-);
-
-export default preferReadonlyParameterTypesRule;
+});
