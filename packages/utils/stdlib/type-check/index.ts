39e705694c89e708acd38f11ef28f5f8caded7ce
noshiro-pf <noshiro.pf@gmail.com>
2022-03-22 01:03:20 +0900

    add test command to utils stdlib

---
+++ packages/utils/stdlib/type-check/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+
+/// <reference no-default-lib="true"/>
+/// <reference path="../dist/lib.dom.d.ts" />
+/// <reference path="../dist/lib.esnext.d.ts" />
+/// <reference path="../dist/utils.d.ts" />
+
+export {};
---
+++ packages/utils/stdlib/type-check/key.ts
@@ -0,0 +1,8 @@
+import { assertType } from './assert-type';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+assertType<ExpectTrue<TypeEq<keyof never, keyof any>>>();
+
+assertType<ExpectTrue<TypeEq<never, keyof unknown>>>();
+
+assertType<ExpectTrue<TypeEq<keyof never, PropertyKey>>>();
