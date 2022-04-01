6cc782f7eb8f05398c312a98ee4a91fa125471a5
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2022-04-01 23:01:51 +0900

    ts-utils: change pipe-class methods to be readonly (#456)

--- packages/utils/ts-utils/src/functional/pipe-class.ts
+++ packages/utils/ts-utils/src/functional/pipe-class.ts
@@ -1,16 +1,21 @@
-class Pipe<A> {
+export const pipe = <A>(a: A): Pipe<A> => new PipeClass(a);
+
+type Pipe<A> = Readonly<{
+  chain: <B>(fn: (a: A) => B) => Pipe<B>;
+  value: A;
+}>;
+
+class PipeClass<A> implements Pipe<A> {
   private readonly a: A;
   constructor(a: A) {
     this.a = a;
   }
 
-  chain<B>(fn: (a: A) => B): Pipe<B> {
-    return new Pipe(fn(this.a));
+  chain<B>(fn: (a: A) => B): PipeClass<B> {
+    return new PipeClass(fn(this.a));
   }
 
   get value(): A {
     return this.a;
   }
 }
-
-export const pipe = <A>(a: A): Pipe<A> => new Pipe(a);
