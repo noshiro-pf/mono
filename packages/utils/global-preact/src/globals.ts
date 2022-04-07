8ef9b9b7551db3e1c44d9b30f497d7a0feefc07f
noshiro-pf <noshiro.pf@gmail.com>
2022-04-08 05:32:39 +0900

    create global-preact

---
+++ packages/utils/global-preact/src/globals.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable functional/immutable-data */
+
+import { useCallback, useEffect, useMemo, useReducer } from 'preact/hooks';
+
+(global as any).useCallback = useCallback;
+(global as any).useEffect = useEffect;
+(global as any).useMemo = useMemo;
+(global as any).useReducer = useReducer;
