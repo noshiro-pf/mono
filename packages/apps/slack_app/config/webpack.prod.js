a5eda2e7c049b89651ea344048afd9fe86816a71
noshiro-pf <noshiro.pf@gmail.com>
2022-04-08 04:13:52 +0900

    global-ts-utils: setup apps: add ProvidePlugin to webpack configs

--- packages/apps/slack_app/config/webpack.prod.js
+++ packages/apps/slack_app/config/webpack.prod.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const { ProvidePlugin } = require('webpack');
+const { providePluginTsUtilsDef } = require('@noshiro/global-ts-utils');
+
 // @ts-check
 
 const {
@@ -13,7 +16,8 @@ console.log('use bundle analyzer: ', dotenvValues.USE_BUNDLE_ANALYZER);
 const webpackConfigMerged = webpackConfigReactProdMaker(
   paths,
   'bundle.js',
-  dotenvValues.USE_BUNDLE_ANALYZER
+  dotenvValues.USE_BUNDLE_ANALYZER,
+  [new ProvidePlugin(providePluginTsUtilsDef)]
 );
 
 module.exports = webpackConfigMerged;
