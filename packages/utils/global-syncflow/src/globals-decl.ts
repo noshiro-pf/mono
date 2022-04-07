33e9f7a9d2120942e79436e2328ac0b02d0ea3ff
noshiro-pf <noshiro.pf@gmail.com>
2022-04-08 05:19:03 +0900

    create global-syncflow

---
+++ packages/utils/global-syncflow/src/globals-decl.ts
@@ -0,0 +1,106 @@
+import type {
+  auditTime as _auditTime,
+  auditTimeI as _auditTimeI,
+  combineLatest as _combineLatest,
+  combineLatestI as _combineLatestI,
+  createBooleanState as _createBooleanState,
+  createEventEmitter as _createEventEmitter,
+  createReducer as _createReducer,
+  createState as _createState,
+  createVoidEventEmitter as _createVoidEventEmitter,
+  debounceTime as _debounceTime,
+  debounceTimeI as _debounceTimeI,
+  distinctUntilChanged as _distinctUntilChanged,
+  distinctUntilChangedI as _distinctUntilChangedI,
+  filter as _filter,
+  fromPromise as _fromPromise,
+  InitializedObservable as _InitializedObservable,
+  map as _map,
+  mapI as _mapI,
+  mapMaybe as _mapMaybe,
+  mapMaybeI as _mapMaybeI,
+  mapResultErr as _mapResultErr,
+  mapResultErrI as _mapResultErrI,
+  mapResultOk as _mapResultOk,
+  mapResultOkI as _mapResultOkI,
+  mapTo as _mapTo,
+  mapToI as _mapToI,
+  mapWithIndex as _mapWithIndex,
+  mapWithIndexI as _mapWithIndexI,
+  Observable as _Observable,
+  of as _of,
+  pairwise as _pairwise,
+  pluck as _pluck,
+  pluckI as _pluckI,
+  scan as _scan,
+  skip as _skip,
+  source as _source,
+  Subscription as _Subscription,
+  throttleTime as _throttleTime,
+  throttleTimeI as _throttleTimeI,
+  unwrapMaybe as _unwrapMaybe,
+  unwrapMaybeI as _unwrapMaybeI,
+  unwrapResultErr as _unwrapResultErr,
+  unwrapResultErrI as _unwrapResultErrI,
+  unwrapResultOk as _unwrapResultOk,
+  unwrapResultOkI as _unwrapResultOkI,
+  withInitialValue as _withInitialValue,
+  withLatestFrom as _withLatestFrom,
+  withLatestFromI as _withLatestFromI,
+  zip as _zip,
+  zipI as _zipI,
+} from '@noshiro/syncflow';
+
+declare global {
+  type InitializedObservable<A> = _InitializedObservable<A>;
+  type Observable<A> = _Observable<A>;
+  type Subscription = _Subscription;
+
+  const auditTime: typeof _auditTime;
+  const auditTimeI: typeof _auditTimeI;
+  const combineLatest: typeof _combineLatest;
+  const combineLatestI: typeof _combineLatestI;
+  const createBooleanState: typeof _createBooleanState;
+  const createEventEmitter: typeof _createEventEmitter;
+  const createReducer: typeof _createReducer;
+  const createState: typeof _createState;
+  const createVoidEventEmitter: typeof _createVoidEventEmitter;
+  const debounceTime: typeof _debounceTime;
+  const debounceTimeI: typeof _debounceTimeI;
+  const distinctUntilChanged: typeof _distinctUntilChanged;
+  const distinctUntilChangedI: typeof _distinctUntilChangedI;
+  const filter: typeof _filter;
+  const fromPromise: typeof _fromPromise;
+  const map: typeof _map;
+  const mapI: typeof _mapI;
+  const mapMaybe: typeof _mapMaybe;
+  const mapMaybeI: typeof _mapMaybeI;
+  const mapResultErr: typeof _mapResultErr;
+  const mapResultErrI: typeof _mapResultErrI;
+  const mapResultOk: typeof _mapResultOk;
+  const mapResultOkI: typeof _mapResultOkI;
+  const mapTo: typeof _mapTo;
+  const mapToI: typeof _mapToI;
+  const mapWithIndex: typeof _mapWithIndex;
+  const mapWithIndexI: typeof _mapWithIndexI;
+  const of: typeof _of;
+  const pairwise: typeof _pairwise;
+  const pluck: typeof _pluck;
+  const pluckI: typeof _pluckI;
+  const scan: typeof _scan;
+  const skip: typeof _skip;
+  const source: typeof _source;
+  const throttleTime: typeof _throttleTime;
+  const throttleTimeI: typeof _throttleTimeI;
+  const unwrapMaybe: typeof _unwrapMaybe;
+  const unwrapMaybeI: typeof _unwrapMaybeI;
+  const unwrapResultErr: typeof _unwrapResultErr;
+  const unwrapResultErrI: typeof _unwrapResultErrI;
+  const unwrapResultOk: typeof _unwrapResultOk;
+  const unwrapResultOkI: typeof _unwrapResultOkI;
+  const withInitialValue: typeof _withInitialValue;
+  const withLatestFrom: typeof _withLatestFrom;
+  const withLatestFromI: typeof _withLatestFromI;
+  const zip: typeof _zip;
+  const zipI: typeof _zipI;
+}
---
+++ packages/utils/global-syncflow/src/globals.ts
@@ -0,0 +1,101 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable functional/immutable-data */
+
+import {
+  auditTime,
+  auditTimeI,
+  combineLatest,
+  combineLatestI,
+  createBooleanState,
+  createEventEmitter,
+  createReducer,
+  createState,
+  createVoidEventEmitter,
+  debounceTime,
+  debounceTimeI,
+  distinctUntilChanged,
+  distinctUntilChangedI,
+  filter,
+  fromPromise,
+  map,
+  mapI,
+  mapMaybe,
+  mapMaybeI,
+  mapResultErr,
+  mapResultErrI,
+  mapResultOk,
+  mapResultOkI,
+  mapTo,
+  mapToI,
+  mapWithIndex,
+  mapWithIndexI,
+  of,
+  pairwise,
+  pluck,
+  pluckI,
+  scan,
+  skip,
+  source,
+  throttleTime,
+  throttleTimeI,
+  unwrapMaybe,
+  unwrapMaybeI,
+  unwrapResultErr,
+  unwrapResultErrI,
+  unwrapResultOk,
+  unwrapResultOkI,
+  withInitialValue,
+  withLatestFrom,
+  withLatestFromI,
+  zip,
+  zipI,
+} from '@noshiro/syncflow';
+
+(global as any).auditTime = auditTime;
+(global as any).auditTimeI = auditTimeI;
+(global as any).combineLatest = combineLatest;
+(global as any).combineLatestI = combineLatestI;
+(global as any).createBooleanState = createBooleanState;
+(global as any).createEventEmitter = createEventEmitter;
+(global as any).createReducer = createReducer;
+(global as any).createState = createState;
+(global as any).createVoidEventEmitter = createVoidEventEmitter;
+(global as any).debounceTime = debounceTime;
+(global as any).debounceTimeI = debounceTimeI;
+(global as any).distinctUntilChanged = distinctUntilChanged;
+(global as any).distinctUntilChangedI = distinctUntilChangedI;
+(global as any).filter = filter;
+(global as any).fromPromise = fromPromise;
+(global as any).map = map;
+(global as any).mapI = mapI;
+(global as any).mapMaybe = mapMaybe;
+(global as any).mapMaybeI = mapMaybeI;
+(global as any).mapResultErr = mapResultErr;
+(global as any).mapResultErrI = mapResultErrI;
+(global as any).mapResultOk = mapResultOk;
+(global as any).mapResultOkI = mapResultOkI;
+(global as any).mapTo = mapTo;
+(global as any).mapToI = mapToI;
+(global as any).mapWithIndex = mapWithIndex;
+(global as any).mapWithIndexI = mapWithIndexI;
+(global as any).of = of;
+(global as any).pairwise = pairwise;
+(global as any).pluck = pluck;
+(global as any).pluckI = pluckI;
+(global as any).scan = scan;
+(global as any).skip = skip;
+(global as any).source = source;
+(global as any).throttleTime = throttleTime;
+(global as any).throttleTimeI = throttleTimeI;
+(global as any).unwrapMaybe = unwrapMaybe;
+(global as any).unwrapMaybeI = unwrapMaybeI;
+(global as any).unwrapResultErr = unwrapResultErr;
+(global as any).unwrapResultErrI = unwrapResultErrI;
+(global as any).unwrapResultOk = unwrapResultOk;
+(global as any).unwrapResultOkI = unwrapResultOkI;
+(global as any).withInitialValue = withInitialValue;
+(global as any).withLatestFrom = withLatestFrom;
+(global as any).withLatestFromI = withLatestFromI;
+(global as any).zip = zip;
+(global as any).zipI = zipI;
