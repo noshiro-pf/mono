8f5716fe04cd9470a461366d6f645ba23d1d1691
noshiro-pf <noshiro.pf@gmail.com>
2022-04-08 05:19:03 +0900

    create global-syncflow

---
+++ packages/utils/global-syncflow/src/index.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-unassigned-import
+import './globals';
+
+export { eslintNoRestrictedImportsSyncflowDef } from './eslint-no-restricted-imports-def';
+export { providePluginSyncflowDef } from './provide-plugin-def';
