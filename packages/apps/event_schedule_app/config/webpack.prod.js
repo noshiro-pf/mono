ee78d3049477b1244d7e78e571609560121d5eca
noshiro-pf <noshiro.pf@gmail.com>
2022-04-09 05:23:07 +0900

    event-schedule-app: setup global utils

--- packages/apps/event_schedule_app/config/webpack.prod.js
+++ packages/apps/event_schedule_app/config/webpack.prod.js
@@ -1,9 +1,27 @@
 'use strict';
 
-const { ProvidePlugin } = require('webpack');
-const { providePluginTsUtilsDef } = require('@noshiro/global-ts-utils');
-
 // @ts-check
+
+const { ProvidePlugin } = require('webpack');
+
+const {
+  providePluginReactDef,
+} = require('@noshiro/global-react/cjs/provide-plugin-def');
+const {
+  providePluginReactUtilsDef,
+} = require('@noshiro/global-react-utils/cjs/provide-plugin-def');
+const {
+  providePluginStyledComponentsDef,
+} = require('@noshiro/global-styled-components/cjs/provide-plugin-def');
+const {
+  providePluginSyncflowDef,
+} = require('@noshiro/global-syncflow/cjs/provide-plugin-def');
+const {
+  providePluginSyncflowReactHooksDef,
+} = require('@noshiro/global-syncflow-react-hooks/cjs/provide-plugin-def');
+const {
+  providePluginTsUtilsDef,
+} = require('@noshiro/global-ts-utils/cjs/provide-plugin-def');
 
 const {
   webpackConfigReactProdMaker,
@@ -17,7 +35,16 @@ const webpackConfigMerged = webpackConfigReactProdMaker(
   paths,
   'bundle.js',
   dotenvValues.USE_BUNDLE_ANALYZER,
-  [new ProvidePlugin(providePluginTsUtilsDef)]
+  [
+    new ProvidePlugin({
+      ...providePluginTsUtilsDef,
+      ...providePluginStyledComponentsDef,
+      ...providePluginReactDef,
+      ...providePluginReactUtilsDef,
+      ...providePluginSyncflowDef,
+      ...providePluginSyncflowReactHooksDef,
+    }),
+  ]
 );
 
 module.exports = webpackConfigMerged;
