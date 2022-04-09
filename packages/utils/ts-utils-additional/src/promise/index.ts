6d0882c4e405db453fb4f7284fc97c687742086f
noshiro-pf <noshiro.pf@gmail.com>
2022-04-10 01:25:12 +0900

    ts-utils-additional: remove duplicated promiseToResult definition

--- packages/utils/ts-utils-additional/src/promise/index.ts
+++ packages/utils/ts-utils-additional/src/promise/index.ts
@@ -1,3 +1,2 @@
 export * from './async-array-filter';
-export * from './promise-to-result';
 export * from './sleep';
--- packages/utils/ts-utils-additional/src/promise/promise-to-result.ts
+++
@@ -1,6 +0,0 @@
-import { Result } from '@noshiro/ts-utils';
-
-export const promiseToResult = <S, E = unknown>(
-  promise: Readonly<Promise<S>>
-): Promise<Result<S, E>> =>
-  promise.then(Result.ok).catch((error) => Result.err(error as E));
