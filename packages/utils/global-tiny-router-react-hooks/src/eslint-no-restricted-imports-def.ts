8143d08b843f6fbd02e46b4e321443af1c681126
noshiro-pf <noshiro.pf@gmail.com>
2022-04-09 16:38:06 +0900

    create global-tiny-router-react-hooks

---
+++ packages/utils/global-tiny-router-react-hooks/src/eslint-no-restricted-imports-def.ts
@@ -0,0 +1,18 @@
+export const eslintNoRestrictedImportsTinyRouterReactHooksDef = {
+  name: '@noshiro/tiny-router-react-hooks',
+  importNames: [
+    'back',
+    'pathname$',
+    'push',
+    'queryParams$',
+    'redirect',
+    'removeListener',
+    'usePathname',
+    'useQueryParams',
+    'useRouterLinkClick',
+    'withSlash',
+    'QueryParams',
+    'Router',
+  ],
+  message: 'use global variable instead.',
+};
