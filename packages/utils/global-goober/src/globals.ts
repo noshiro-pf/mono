0ef5a9c683bfefbf87fef1ea46624f58732b27fe
noshiro-pf <noshiro.pf@gmail.com>
2022-04-11 00:48:48 +0900

    create global-goober

---
+++ packages/utils/global-goober/src/globals.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable functional/immutable-data */
+
+import { styled as _styled } from '@noshiro/goober';
+
+(global as any).styled = _styled;
---
+++ packages/utils/global-goober/src/provide-plugin-def.ts
@@ -0,0 +1,3 @@
+export const providePluginGooberDef = {
+  styled: ['@noshiro/goober', 'styled'],
+};
