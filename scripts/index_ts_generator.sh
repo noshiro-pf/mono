ab71dc5f824a71752aa594f364b37777f0de2e26
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2022-04-13 12:49:10 +0900

    lint fix index_ts_generator.sh (#473)

--- scripts/index_ts_generator.sh
+++ scripts/index_ts_generator.sh
@@ -2,7 +2,7 @@
 
 # index_ts_generator.sh
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$(cd $(dirname "$0") || exit; pwd)
 
 # configs
 ADD_SUB_DIRECTORY_EXPORT_IN_INDEX_TS="true"
@@ -17,41 +17,41 @@ ignore=""
 
 while [[ $# -gt 0 ]]
 do
-  key="$1"
+    key="$1"
 
-  case $key in
-    --clear)
-    clear=true
-    shift # past argument
-    ;;
-    --min-depth)
-    min_recursion_depth=$2
-    shift # past argument
-    shift # past argument
-    ;;
-    --max-depth)
-    max_recursion_depth=$2
-    shift # past argument
-    shift # past argument
-    ;;
-    --ignore)
-    ignore=$2
-    shift # past argument
-    shift # past argument
-    ;;
-    *)    # unknown option
-    shift # past argument
-    ;;
-  esac
+    case $key in
+        --clear)
+            clear=true
+            shift # past argument
+        ;;
+        --min-depth)
+            min_recursion_depth="$2"
+            shift # past argument
+            shift # past argument
+        ;;
+        --max-depth)
+            max_recursion_depth="$2"
+            shift # past argument
+            shift # past argument
+        ;;
+        --ignore)
+            ignore="$2"
+            shift # past argument
+            shift # past argument
+        ;;
+        *)    # unknown option
+            shift # past argument
+        ;;
+    esac
 done
 
 echo clear: ${clear}
-echo min_recursion_depth: ${min_recursion_depth}
-echo max_recursion_depth: ${max_recursion_depth}
-echo ignore: ${ignore}
+echo min_recursion_depth: "${min_recursion_depth}"
+echo max_recursion_depth: "${max_recursion_depth}"
+echo ignore: "${ignore}"
 
 # move to target directory
-cd ${target_directory}
+cd "${target_directory}" || exit
 echo -n "target_directory: "
 pwd
 echo
@@ -59,61 +59,61 @@ echo
 index_ts_files=()
 
 # generate index.ts recursively
-for directory in $(find . -mindepth ${min_recursion_depth} -maxdepth ${max_recursion_depth} -type d); do
-  echo ${directory}
+for directory in $(find . -mindepth "${min_recursion_depth}" -maxdepth "${max_recursion_depth}" -type d); do
+    echo "${directory}"
 
-  if [ -n "${ignore}" ]; then
-    if [[ "${directory}" =~ "${ignore}" ]]; then
-      echo "skipped."
-      continue
-    fi
-  fi
-
-  index_ts="${directory}/index.ts"
-
-  if "${clear}"; then
-    rm ${index_ts}
-  else
-
-    result=""
-
-    # files in a current directory
-    for file in $(find ${directory} -mindepth 1 -maxdepth 1 -type f); do
-      filename=$(basename ${file})
-
-      # filename without extension
-      filename_without_ext="${filename%.*}"
-
-      # only files that match a regular expression "[a-zA-Z0-9_]+.ts"
-      if [ ${filename} != "index.ts" ]; then
-        if [[ ${filename} =~ ${TS_FILENAME_REGEX} ]]; then
-          # add line "export * from 'filename'"
-          result+="export * from './${filename_without_ext}';"
+    if [ -n "${ignore}" ]; then
+        if [[ "${directory}" =~ ${ignore} ]]; then
+            echo "skipped."
+            continue
         fi
-      fi
-    done
-
-    if [ ${ADD_SUB_DIRECTORY_EXPORT_IN_INDEX_TS} = "true" ]; then
-      # directories in a current directory
-      for sub_directory in $(find ${directory} -mindepth 1 -maxdepth 1 -type d); do
-        # add line "export * from 'directory_name'"
-        sub_directory_basename=$(basename ${sub_directory})
-        result+="export * from './${sub_directory_basename}';"
-      done
     fi
 
-    echo "${result}" > ${index_ts}
-    index_ts_files+=" ${index_ts}"
+    index_ts="${directory}/index.ts"
 
-  fi
+    if "${clear}"; then
+        rm "${index_ts}"
+    else
 
-  echo ${directory} "done"
+        result=""
+
+        # files in a current directory
+        for file in $(find "${directory}" -mindepth 1 -maxdepth 1 -type f); do
+            filename=$(basename "${file}")
+
+            # filename without extension
+            filename_without_ext="${filename%.*}"
+
+            # only files that match a regular expression "[a-zA-Z0-9_]+.ts"
+            if [ "${filename}" != "index.ts" ]; then
+                if [[ ${filename} =~ ${TS_FILENAME_REGEX} ]]; then
+                    # add line "export * from 'filename'"
+                    result+="export * from './${filename_without_ext}';"
+                fi
+            fi
+        done
+
+        if [ ${ADD_SUB_DIRECTORY_EXPORT_IN_INDEX_TS} = "true" ]; then
+            # directories in a current directory
+            for sub_directory in $(find "${directory}" -mindepth 1 -maxdepth 1 -type d); do
+                # add line "export * from 'directory_name'"
+                sub_directory_basename=$(basename "${sub_directory}")
+                result+="export * from './${sub_directory_basename}';"
+            done
+        fi
+
+        echo "${result}" > "${index_ts}"
+        index_ts_files+=" ${index_ts}"
+
+    fi
+
+    echo "${directory}" "done"
 
 done
 
-echo 
+echo
 echo "--- prettier ---"
 
 if [ -n "${index_ts_files}" ]; then
-  node ${SCRIPT_DIR}/../node_modules/.bin/prettier --write ${index_ts_files}
+    node "${SCRIPT_DIR}"/../node_modules/.bin/prettier --write ${index_ts_files}
 fi
