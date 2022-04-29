f98e573fbac32f4d8663261937d8c3b61c820beb
noshiro-pf <noshiro.pf@gmail.com>
2022-04-29 21:48:36 +0900

    event-schedule-app: setup firebase emulator

--- packages/apps/event_schedule_app/src/initialize-firebase.ts
+++ packages/apps/event_schedule_app/src/initialize-firebase.ts
@@ -4,21 +4,30 @@ import {
 } from '@noshiro/event-schedule-app-shared';
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { collection, initializeFirestore } from 'firebase/firestore';
+import {
+  collection,
+  connectFirestoreEmulator,
+  initializeFirestore,
+} from 'firebase/firestore';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
+import firebaseJson from '../firebase.json';
 import { isDevelopment } from './env';
 import { clog } from './utils';
-// import firebasejson from '../firebase.json';
 
 const app = initializeApp(firebaseConfig);
 
 clog('firebase.initializeApp done.');
 
+clog(hasKey(window, 'Cypress'));
+
 export const db = initializeFirestore(app, {
   ignoreUndefinedProperties: true,
+  // https://zenn.dev/cauchye/articles/20210816_yutaro-elk
+  experimentalForceLongPolling: hasKey(window, 'Cypress'),
 });
 // if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
 //   const host = 'localhost';
-//   const port = firebasejson.emulators.firestore.port;
+//   const port = firebaseJson.emulators.firestore.port;
 //   // db.useEmulator(host, port);
 //   db.settings({ host: `${host}:${port}`, ssl: false });
 //   clog(`using firestore emulator. (${host}:${port})`);
@@ -32,3 +41,22 @@ export const dbEvents = collection(
 export const auth = getAuth();
 
 export const googleAuthProvider = new GoogleAuthProvider();
+
+if (isDevelopment) {
+  connectFirestoreEmulator(
+    db,
+    'localhost',
+    firebaseJson.emulators.firestore.port
+  );
+  // connectAuthEmulator(
+  //   auth,
+  //   `localhost:${firebaseJson.emulators.auth.port.toString()}`
+  // );
+
+  const functions = getFunctions(app);
+  connectFunctionsEmulator(
+    functions,
+    'localhost',
+    firebaseJson.emulators.functions.port
+  );
+}
