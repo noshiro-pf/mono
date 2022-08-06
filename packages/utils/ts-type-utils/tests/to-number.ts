afc303ddbd8444ddfe83e2788fe130c1007e6477
noshiro-pf <noshiro.pf@gmail.com>
2022-08-06 15:21:17 +0900

    ts-type-utils: improve ToNumber implementation

--- packages/utils/ts-type-utils/tests/to-number.ts
+++ packages/utils/ts-type-utils/tests/to-number.ts
@@ -4,4 +4,4 @@ import { assertType } from './assert-type';
 assertType<TypeEq<ToNumber<'1000'>, 1000>>();
 assertType<TypeEq<ToNumber<'8192'>, 8192>>();
 assertType<TypeEq<ToNumber<'9999'>, 9999>>();
-assertType<TypeEq<ToNumber<'10000'>, '10000'>>();
+assertType<TypeEq<ToNumber<'10000'>, 10_000>>();
