fb701bee312789c6b42bfe9a8175e740f5043695
noshiro-pf <noshiro.pf@gmail.com>
2022-10-04 11:06:11 +0900

    refactor: [syncflow] improve type annotation for idMaker

--- packages/utils/syncflow/src/core/utils/idmaker.ts
+++ packages/utils/syncflow/src/core/utils/idmaker.ts
@@ -1,19 +1,21 @@
 import type { ObservableId, SubscriberId, Token } from '../types';
 
-function* idMaker(): Generator<number, number, number> {
-  let mut_i = 0;
+function* idMaker<T extends number>(): Generator<T, T, T> {
+  let mut_i: T = 0 as T;
   while (true) {
     yield mut_i;
-    mut_i += 1;
+    mut_i = (mut_i + 1) as T;
   }
 }
 
-const observableIdMaker = idMaker();
-const subscriberIdMaker = idMaker();
-const tokenMaker = idMaker();
+const observableIdMaker = idMaker<ObservableId>();
+const subscriberIdMaker = idMaker<SubscriberId>();
+const tokenMaker = idMaker<Token>();
 
 export const issueObservableId = (): ObservableId =>
-  observableIdMaker.next().value as ObservableId;
+  observableIdMaker.next().value;
+
 export const issueSubscriberId = (): SubscriberId =>
-  subscriberIdMaker.next().value as SubscriberId;
-export const issueToken = (): Token => tokenMaker.next().value as Token;
+  subscriberIdMaker.next().value;
+
+export const issueToken = (): Token => tokenMaker.next().value;
