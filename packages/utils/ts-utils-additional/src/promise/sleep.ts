44a6bb6c6c689f45891ba12df38699eaebb3e2e9
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2022-10-18 00:14:21 +0900

    feat: [ts-utils-additional] add abortFn to return value of sleep (#592)

--- packages/utils/ts-utils-additional/src/promise/sleep.ts
+++ packages/utils/ts-utils-additional/src/promise/sleep.ts
@@ -1,7 +1,23 @@
 /**
- * TODO: cancel できるようにする
+ *
+ * @param milliSec
+ * @returns [Promise, interruptSleepFunction]
  */
-export const sleep = (milliSec: number): Promise<unknown> =>
-  new Promise((resolve) => {
-    setTimeout(resolve, milliSec);
+export const sleep = (
+  milliSec: number
+): readonly [Promise<unknown>, () => void] => {
+  let mut_timer: TimerId | undefined = undefined;
+  let mut_resolver: ((value: unknown) => void) | undefined = undefined;
+
+  const promise = new Promise((resolve) => {
+    mut_resolver = resolve;
+    mut_timer = setTimeout(resolve, milliSec);
   });
+
+  const interruptSleep = (): void => {
+    clearTimeout(mut_timer);
+    mut_resolver?.(undefined);
+  };
+
+  return [promise, interruptSleep];
+};
