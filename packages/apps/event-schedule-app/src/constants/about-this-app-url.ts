9364b8e64dc500591dc281a9be76a3cadf66d960
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2022-10-25 02:25:54 +0900

    fix: [event-schedule-app] fix notion url (#597)

--- packages/apps/event-schedule-app/src/constants/about-this-app-url.ts
+++ packages/apps/event-schedule-app/src/constants/about-this-app-url.ts
@@ -1,7 +1,7 @@
 export const aboutThisAppUrl =
-  'https://noshiro.notion.site/c40d53907f204a3e9961a4a2b623ca22';
+  'https://noshiro.notion.site/71b7d1ccf29945c0a1a086e1a030a91a';
 
 export const introductionUrl =
-  'https://noshiro.notion.site/899268cf44c54eefa564543c63864f2a';
+  'https://noshiro.notion.site/45b55a4512b14e32a62f5a092c61fda1';
 
 export const feedbackUrl = 'https://notionforms.io/forms/-717';
