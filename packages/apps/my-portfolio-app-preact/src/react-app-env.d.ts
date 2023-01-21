7fa45b6e2313f8a1912904e5e8878551fa8cbfaf
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2023-01-21 20:09:14 +0900

    refactor: remove unnecessary eslint-disable comment (#645)

--- packages/apps/my-portfolio-app-preact/src/react-app-env.d.ts
+++ packages/apps/my-portfolio-app-preact/src/react-app-env.d.ts
@@ -7,7 +7,7 @@
 /// <reference path="../../../utils/global-ts-utils/esm/globals-decl.d.ts" />
 
 /// <reference types="node" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
