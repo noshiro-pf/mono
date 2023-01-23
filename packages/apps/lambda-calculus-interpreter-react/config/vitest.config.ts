c13be3b2ef7d5199a2d68daf898ea63702da8b4d
noshiro-pf <noshiro.pf@gmail.com>
2023-01-23 22:26:31 +0900

    feat: use Vite in lambda-calculus-interpreter-react

---
+++ packages/apps/lambda-calculus-interpreter-react/config/vitest.config.ts
@@ -0,0 +1,27 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vitest/config';
+
+const thisDir = dirname(fileURLToPath(import.meta.url));
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: [
+      '@noshiro/global-react',
+      '@noshiro/global-react-utils',
+      '@noshiro/global-styled-components',
+      '@noshiro/global-syncflow',
+      '@noshiro/global-syncflow-react-hooks',
+      '@noshiro/global-tiny-router-observable',
+      '@noshiro/global-tiny-router-react-hooks',
+      '@noshiro/global-ts-utils',
+      resolve(thisDir, 'globals.ts'),
+    ],
+    typecheck: {
+      tsconfig: resolve(thisDir, 'tsconfig.test.json'),
+    },
+  },
+});
