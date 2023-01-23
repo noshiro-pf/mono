fc11a183b40b7fad78cd5df34b815bc649fc6984
noshiro-pf <noshiro.pf@gmail.com>
2023-01-23 22:10:02 +0900

    feat: use Vite in blueprintjs-playground

--- packages/apps/blueprintjs-playground/.eslintrc.cjs
+++ packages/apps/blueprintjs-playground/.eslintrc.cjs
@@ -1,8 +1,53 @@
 'use strict';
-
 // @ts-check
 
 /** @typedef { import("eslint").Linter.Config } LinterConfig */
+/** @typedef { import("../../../config/eslintrc/eslint-rules/rules-type/typescript-eslint-rules").TypeScriptEslintRules } TypeScriptEslintRules */
+
+const {
+  restrictedImportsOption,
+} = require('../../../config/eslintrc/eslint-rules');
+
+const {
+  eslintNoRestrictedImportsTsUtilsDef,
+} = require('@noshiro/global-ts-utils/cjs/eslint-no-restricted-imports-def');
+const {
+  eslintNoRestrictedImportsReactUtilsDef,
+} = require('@noshiro/global-react-utils/cjs/eslint-no-restricted-imports-def');
+const {
+  eslintNoRestrictedImportsSyncflowDef,
+} = require('@noshiro/global-syncflow/cjs/eslint-no-restricted-imports-def');
+const {
+  eslintNoRestrictedImportsSyncflowReactHooksDef,
+} = require('@noshiro/global-syncflow-react-hooks/cjs/eslint-no-restricted-imports-def');
+const {
+  eslintNoRestrictedImportsReactDef,
+} = require('@noshiro/global-react/cjs/eslint-no-restricted-imports-def');
+const {
+  eslintNoRestrictedImportsTinyRouterReactHooksDef,
+} = require('@noshiro/global-tiny-router-react-hooks/cjs/eslint-no-restricted-imports-def');
+const {
+  eslintNoRestrictedImportsStyledComponentsDef,
+} = require('@noshiro/global-styled-components/cjs/eslint-no-restricted-imports-def');
+
+const { join } = require('path');
+
+/** @type {TypeScriptEslintRules["@typescript-eslint/no-restricted-imports"]} */
+const noRestrictedImports = [
+  'warn',
+  {
+    paths: [
+      ...restrictedImportsOption.paths,
+      eslintNoRestrictedImportsTsUtilsDef,
+      eslintNoRestrictedImportsReactUtilsDef,
+      eslintNoRestrictedImportsSyncflowDef,
+      eslintNoRestrictedImportsSyncflowReactHooksDef,
+      eslintNoRestrictedImportsReactDef,
+      eslintNoRestrictedImportsTinyRouterReactHooksDef,
+      eslintNoRestrictedImportsStyledComponentsDef,
+    ],
+  },
+];
 
 /** @type {LinterConfig} */
 const config = {
@@ -12,7 +57,15 @@ const config = {
     tsconfigRootDir: __dirname,
   },
   rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: [join(__dirname, '../../../'), '.'],
+      },
+    ],
     'import/no-internal-modules': 'off',
+    '@typescript-eslint/no-restricted-imports': noRestrictedImports,
+    '@typescript-eslint/no-namespace': 'error', // enable in Vite project
   },
 };
 
--- packages/apps/blueprintjs-playground/src/main.tsx
+++ packages/apps/blueprintjs-playground/src/main.tsx
@@ -1,45 +1,11 @@
-import { InputGroup, NumericInput } from '@blueprintjs/core';
-import { memoNamed } from '@noshiro/react-utils';
-import styled from 'styled-components';
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import { App } from './app';
+import './index.css';
 
-export const Main = memoNamed('Main', () => (
-  <Root>
-    <h1>{'Blueprint.js Playground'}</h1>
-
-    <h2>{'NumericInput'}</h2>
-    <MarginBox>
-      <NumericInput disabled={false} fill={true} value={0} />
-    </MarginBox>
-    <MarginBox>
-      <NumericInput disabled={true} fill={true} value={0} />
-    </MarginBox>
-
-    <h2>{'InputGroup'}</h2>
-    <MarginBox>
-      <InputGroup value={'aaa'} onChange={noop} />
-    </MarginBox>
-    <MarginBox>
-      <InputGroup disabled={true} value={'aaa'} onChange={noop} />
-    </MarginBox>
-    <MarginBox>
-      <InputGroup
-        placeholder={'placeholder'}
-        value={undefined}
-        onChange={noop}
-      />
-    </MarginBox>
-  </Root>
-));
-
-const Root = styled.div`
-  min-height: 100vh;
-  padding: 20px;
-`;
-
-const MarginBox = styled.div`
-  margin: 10px;
-
-  & > * {
-    margin: 10px;
-  }
-`;
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.querySelector('#root')
+);
