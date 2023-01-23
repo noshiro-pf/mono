1f2ebff7f29e15048e60757ffeb170bc5b79e0c4
noshiro-pf <noshiro.pf@gmail.com>
2023-01-24 04:01:02 +0900

    infra: use vitest in utils

---
+++ packages/utils/syncflow/config/vitest.config.ts
@@ -0,0 +1,16 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vitest/config';
+
+const thisDir = dirname(fileURLToPath(import.meta.url));
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  test: {
+    globals: true,
+    typecheck: {
+      tsconfig: resolve(thisDir, 'tsconfig.test.json'),
+    },
+    testTimeout: 30000,
+  },
+});
