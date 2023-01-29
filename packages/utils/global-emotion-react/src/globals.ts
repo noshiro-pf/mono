e2afa0bbd7ccc5dea939196c70a69d4650c76432
noshiro-pf <noshiro.pf@gmail.com>
2023-01-30 02:40:33 +0900

    feat: create global-emotion-react

---
+++ packages/utils/global-emotion-react/src/globals.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable functional/immutable-data */
+
+import { css as _css } from '@emotion/react';
+
+(global as any).css = _css;
