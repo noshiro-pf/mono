59bbc09e9b6cb44ebd775ebbe4388367c3ade5a0
noshiro-pf <noshiro.pf@gmail.com>
2023-02-28 03:56:10 +0900

    test: add e2e test for catan-dice-app

--- packages/apps/catan-dice-app/src/components/histogram/histogram-view.tsx
+++ packages/apps/catan-dice-app/src/components/histogram/histogram-view.tsx
@@ -24,6 +24,7 @@ export const HistogramView = memoNamed<Props>(
           css={css`
             padding: 5px;
           `}
+          data-cy={'roll-count'}
         >
           {'N = '}
           {numSample}
