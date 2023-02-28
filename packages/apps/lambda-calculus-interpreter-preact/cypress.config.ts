6b06cfa46cbaf1fd2e9a9fcb81d16c2dcf402add
noshiro-pf <noshiro.pf@gmail.com>
2023-02-28 10:53:59 +0900

    feat: extend pageLoadTimeout in cypress.config

--- packages/apps/lambda-calculus-interpreter-preact/cypress.config.ts
+++ packages/apps/lambda-calculus-interpreter-preact/cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   projectId: '3w8af2',
   viewportWidth: 375,
   viewportHeight: 667,
+  modifyObstructiveCode: false,
+  pageLoadTimeout: 120000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- packages/apps/lambda-calculus-interpreter-react/cypress.config.ts
+++ packages/apps/lambda-calculus-interpreter-react/cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   projectId: 'ixxvs7',
   viewportWidth: 375,
   viewportHeight: 667,
+  modifyObstructiveCode: false,
+  pageLoadTimeout: 120000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
