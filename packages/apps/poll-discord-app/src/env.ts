a944b5953f5f2ace0510086371f58945a1fbe61c
noshiro-pf <noshiro.pf@gmail.com>
2023-03-23 11:35:10 +0900

    feat: poll-discord-app: console.log NODE_ENV

--- packages/apps/poll-discord-app/src/env.ts
+++ packages/apps/poll-discord-app/src/env.ts
@@ -7,4 +7,6 @@ export const DISCORD_TOKEN = process.env['DISCORD_TOKEN'] ?? '';
 
 export const isDev = process.env['NODE_ENV'] === 'development';
 
+console.log(`NODE_ENV = "${process.env['NODE_ENV'] ?? ''}"`);
+
 export const useEmulators: boolean = false as boolean;
