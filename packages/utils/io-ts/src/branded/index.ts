46f003ba8fbb2405e403d25e3bde180823de4a73
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2023-03-25 18:31:02 +0900

    feat: io-ts: add simpleBrandedString utility (#768)

--- packages/utils/io-ts/src/branded/index.ts
+++ packages/utils/io-ts/src/branded/index.ts
@@ -3,4 +3,6 @@ export * from './int';
 export * from './non-negative-number';
 export * from './positive-integer';
 export * from './positive-number';
+export * from './simple-branded-number';
+export * from './simple-branded-string';
 export * from './uint';
