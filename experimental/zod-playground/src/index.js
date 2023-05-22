a58ed3a5576dbca29758f968cc38e70b2fc169f3
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2023-05-22 18:54:54 +0900

    add zod-playground to experimental (#833)

---
+++ experimental/zod-playground/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toInt = exports.isInt = void 0;
+const tslib_1 = require("tslib");
+const z = tslib_1.__importStar(require("zod"));
+const Int = z
+    .number()
+    .refine((a) => Number.isInteger(a), (a) => ({ message: `Invalid value "${a}" supplied to Int` }))
+    .brand('Int');
+const isInt = (a) => Int.safeParse(a).success;
+exports.isInt = isInt;
+const toInt = (a) => Int.parse(a);
+exports.toInt = toInt;
