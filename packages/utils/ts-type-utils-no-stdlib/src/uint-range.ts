f5427a1d4a527bbc1f41ef34079b8013f7e27b16
noshiro-pf <noshiro.pf@gmail.com>
2023-06-21 15:22:00 +0900

    feat: ts-type-utils: add UintRangeInclusive

--- packages/utils/ts-type-utils-no-stdlib/src/uint-range.ts
+++ packages/utils/ts-type-utils-no-stdlib/src/uint-range.ts
@@ -1,7 +1,16 @@
+import { type IndexOfTuple } from './index-of-tuple';
 import { type Index } from './index-type';
+import { type MakeTuple } from './make-tuple';
 import { type RelaxedExclude } from './utils';
 
 export type UintRange<
   Start extends number,
   End extends number
 > = RelaxedExclude<Index<End>, Index<Start>>;
+
+type IndexInclusive<N extends number> = IndexOfTuple<[...MakeTuple<0, N>, 0]>;
+
+export type UintRangeInclusive<
+  MinValue extends number,
+  MaxValue extends number
+> = RelaxedExclude<IndexInclusive<MaxValue>, Index<MinValue>>;
