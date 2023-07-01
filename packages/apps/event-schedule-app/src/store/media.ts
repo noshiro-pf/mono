c76ee2a0fb6c865bf576b828a3c9e21b89c26596
noshiro-pf <noshiro.pf@gmail.com>
2023-07-01 22:33:58 +0900

    feat: event-schedule-app: hide datetime shortcuts on mobile

---
+++ packages/apps/event-schedule-app/src/store/media.ts
@@ -0,0 +1,11 @@
+const mql = window.matchMedia('(max-width: 600px)');
+
+const { state$: isMobile$, setState: setIsMobile } = createBooleanState(false);
+
+mql.addEventListener('change', (a) => {
+  setIsMobile(a.matches);
+});
+
+const useIsMobile = (): boolean => useObservableValue(isMobile$);
+
+export { useIsMobile };
