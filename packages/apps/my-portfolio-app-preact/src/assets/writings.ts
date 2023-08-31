98c4a7c23712372c679b429eb679de5d0d668d10
noshiro-pf <noshiro.pf@gmail.com>
2023-09-01 04:25:18 +0900

    feat: update portfolio contents

--- packages/apps/my-portfolio-app-preact/src/assets/writings.ts
+++ packages/apps/my-portfolio-app-preact/src/assets/writings.ts
@@ -2,6 +2,50 @@ import { type WritingsInfo } from '../types';
 
 export const writings: readonly WritingsInfo[] = [
   {
+    id: 'event-schedule-app',
+    link: 'https://noshiro.notion.site/71b7d1ccf29945c0a1a086e1a030a91a',
+    title: '★ 自作アプリ「イベント日程調整アプリ」の紹介',
+    subtitle:
+      'イベント日程調整アプリ（https://event-schedule-app.web.app/）の機能の紹介ページ',
+    body: '',
+  },
+  {
+    id: 'syncflow-slides',
+    link: 'https://docs.google.com/presentation/d/1y9F5jxD6e1bFzLOs3BVAzIqhW806OfmLzIYaaU1j7yM/edit#slide=id.p',
+    title: '★ リアクティブプログラミングライブラリ「SyncFlow」を自作した話',
+    subtitle:
+      '私がウェブフロントエンドの状態管理に使用している自作ライブラリの紹介',
+    body: 'RxJSで発生するglitchという現象の発生原因とその対処法、他のglitch-freeリアクティブプログラミングライブラリがどう解決しているかを論文等を参照しつつ紹介。最後に拙作「SyncFlow」の実装概要を説明。',
+  },
+  {
+    id: 'reactive-programming-introduction-with-rxjs',
+    link: 'https://qiita.com/pikohideaki/items/292ab134397f4959e66b',
+    title: '★ リアクティブプログラミングはどう有用なのか',
+    subtitle: 'リアクティブプログラミングについての入門者向け解説記事',
+    body: 'リアクティブプログラミングとは何なのか・どうやって使うのか・使うと何が嬉しいのかを RxJS を題材になるべく予備知識なく分かるように解説してみたもの。',
+  },
+  {
+    id: 'io-ts',
+    link: 'https://docs.google.com/presentation/d/19YwEkeNl8x3IWglTv8YjcOZPyMVympXn0qyDLf2mXUI/edit#slide=id.p',
+    title: 'io-ts のようなライブラリを自作した',
+    subtitle: '自作ライブラリ "@noshiro/io-ts" の紹介スライド',
+    body: 'TypeScript で型とバリデーター関数をより安全に効率良く実装するためのライブラリを作りました。',
+  },
+  {
+    id: 'numeric-input-interface',
+    link: 'https://zenn.dev/noshiro_piko/articles/numeric-input-interface',
+    title: 'numeric input の React コンポーネントのインターフェース考察',
+    subtitle: '',
+    body: 'ウェブUIにおける一般的な数値入力欄をReactコンポーネントとして定義する際のインターフェース・状態管理方法について考えまとめました。',
+  },
+  {
+    id: 'typescript-type-branding',
+    link: 'https://zenn.dev/noshiro_piko/articles/typescript-branded-type-int',
+    title: '★ TypeScript の Type Branding をより便利に活用する方法のまとめ',
+    subtitle: '',
+    body: 'TypeScript で用いられることのある Type Branding というハックをより便利に活用する方法をまとめました。',
+  },
+  {
     id: 'typescript-type-utilities-min-max',
     link: 'https://zenn.dev/noshiro_piko/articles/typescript-type-utilities',
     title: 'TypeScript 型ユーティリティ集',
@@ -16,11 +60,11 @@ export const writings: readonly WritingsInfo[] = [
     body: '',
   },
   {
-    id: 'typescript-type-branding',
-    link: 'https://zenn.dev/noshiro_piko/articles/typescript-branded-type-int',
-    title: '★ TypeScript の Type Branding をより便利に活用する方法のまとめ',
+    id: 'type-challenges-introduction',
+    link: 'https://docs.google.com/presentation/d/1uyL1tUJkpL8kgoNwpSy28sjCMHNzsYiO1aPC4_yzTYs/edit?usp=sharing',
+    title: '★ type challenges の紹介',
     subtitle: '',
-    body: 'TypeScript で用いられることのある Type Branding というハックをより便利に活用する方法をまとめました。',
+    body: 'TypeScriptの型パズル集から、個人的に役に立った知識が多く詰まった問題を1問選んで解説してみました。',
   },
   {
     id: 'tatekae',
@@ -28,21 +72,6 @@ export const writings: readonly WritingsInfo[] = [
     title: '最短手数の立替精算方法を計算するアルゴリズム',
     subtitle:
       '旅先で立て替えた支払いの最適な精算方法を計算する効率の良いアルゴリズムについて',
-    body: '',
-  },
-  {
-    id: 'io-ts',
-    link: 'https://docs.google.com/presentation/d/19YwEkeNl8x3IWglTv8YjcOZPyMVympXn0qyDLf2mXUI/edit#slide=id.p',
-    title: 'io-ts のようなライブラリを自作した',
-    subtitle: '自作ライブラリ "@noshiro/io-ts" の紹介スライド',
-    body: '',
-  },
-  {
-    id: 'event-schedule-app',
-    link: 'https://noshiro.notion.site/71b7d1ccf29945c0a1a086e1a030a91a',
-    title: '★ 自作アプリ「イベント日程調整アプリ」の紹介',
-    subtitle:
-      'イベント日程調整アプリ（https://event-schedule-app.web.app/）の機能の紹介ページ',
     body: '',
   },
   {
@@ -61,13 +90,6 @@ export const writings: readonly WritingsInfo[] = [
     body: 'Union型を内部に含むタプルやレコード型の代入可能性判定で遭遇しうる型チェックの制限について、TypeScriptのコンパイラのソースコードを読んで調べたので紹介します。',
   },
   {
-    id: 'type-challenges-introduction',
-    link: 'https://docs.google.com/presentation/d/1uyL1tUJkpL8kgoNwpSy28sjCMHNzsYiO1aPC4_yzTYs/edit?usp=sharing',
-    title: '★ type challenges の紹介',
-    subtitle: '',
-    body: 'TypeScriptの型パズル集から、個人的に役に立った知識が多く詰まった問題を1問選んで解説してみました。',
-  },
-  {
     id: 'ts4.1-immutable-setin',
     link: 'https://www.kabuku.co.jp/developers/typescript-strictly-typed-immutable-library',
     title:
@@ -78,19 +100,11 @@ export const writings: readonly WritingsInfo[] = [
   },
   {
     id: 'about-noUncheckedIndexedAccess',
-    link: 'https://www.notion.so/noshiro/TypeScript-v4-1-noUncheckedIndexedAccess-00144ca0b4f44c1cb73f7ca43002aa70',
+    link: 'https://noshiro.notion.site/TypeScript-v4-1-noUncheckedIndexedAccess-00144ca0b4f44c1cb73f7ca43002aa70?pvs=4',
     title:
       'TypeScript v4.1 で追加されたnoUncheckedIndexedAccessオプションを有効にして使いこなす方法',
     subtitle: '',
     body: 'TSの新コンパイラオプションnoUncheckedIndexedAccessを使う際に便利なユーティリティの紹介です。',
-  },
-  {
-    id: 'syncflow-slides',
-    link: 'https://docs.google.com/presentation/d/1y9F5jxD6e1bFzLOs3BVAzIqhW806OfmLzIYaaU1j7yM/edit#slide=id.p',
-    title: '★ リアクティブプログラミングライブラリ「SyncFlow」を自作した話',
-    subtitle:
-      'RxJSで発生するglitchという不健全な挙動と、拙作「SyncFlow」の紹介',
-    body: 'RxJSで発生するglitchという現象の発生原因とその対処法、他のglitch-freeリアクティブプログラミングライブラリがどう解決しているかを論文等を参照しつつ紹介。最後に拙作「SyncFlow」の実装概要を説明。',
   },
   {
     id: 'color-contrast-qiita',
@@ -106,14 +120,7 @@ export const writings: readonly WritingsInfo[] = [
     title: '簡易リアクティブプログラミングライブラリの作り方（TypeScript）',
     subtitle:
       'TypeScriptで簡単なリアクティブプログラミングライブラリっぽいものをスクラッチで実装する',
-    body: 'RxJSのようなリアクティブプログラミングができるライブラリの簡易版を実装し、敷居を下げるために書いたもの。',
-  },
-  {
-    id: 'reactive-programming-introduction-with-rxjs',
-    link: 'https://qiita.com/pikohideaki/items/292ab134397f4959e66b',
-    title: '★ リアクティブプログラミングはどう有用なのか',
-    subtitle: 'リアクティブプログラミングについての入門者向け解説記事',
-    body: 'リアクティブプログラミングとは何なのか・どうやって使うのか・使うと何が嬉しいのかを RxJS を題材になるべく予備知識なく分かるように解説してみたもの。',
+    body: 'RxJSのようなリアクティブプログラミングができるライブラリの簡易版を実装し、誰でも基本的な動作を理解できるようにするために書いたもの。',
   },
   {
     id: 'dezero-slides',
