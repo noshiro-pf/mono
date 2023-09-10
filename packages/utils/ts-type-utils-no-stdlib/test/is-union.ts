efe40cad4fb8fef7cbf1b6d2a702c92c4fa34bc5
noshiro-pf <noshiro.pf@gmail.com>
2023-09-10 11:12:18 +0900

    fix: ts-type-utils-no-stdlib: fix IsUnion implementation

--- packages/utils/ts-type-utils-no-stdlib/test/is-union.ts
+++ packages/utils/ts-type-utils-no-stdlib/test/is-union.ts
@@ -5,3 +5,20 @@ expectType<IsUnion<never>, false>('=');
 expectType<IsUnion<string>, false>('=');
 expectType<IsUnion<number | string>, true>('=');
 expectType<IsUnion<[number | string]>, false>('=');
+
+expectType<
+  IsUnion<Readonly<{ a: 0 }> | Readonly<{ a: 1; b: 1 }> | Readonly<{ b: 2 }>>,
+  true
+>('=');
+
+expectType<IsUnion<Record<number, number> | Record<string, number>>, true>('=');
+
+expectType<
+  IsUnion<
+    | Readonly<{ a: 0 }>
+    | Readonly<{ a: 1; b: 1 }>
+    | Readonly<{ b: 2 }>
+    | Record<string, number>
+  >,
+  true
+>('=');
