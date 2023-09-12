6927d7616bc2123e3bbe0f8153682340240499f0
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2023-09-12 10:38:07 +0900

    stdlib: improve object.hasOwn return type (#941)

--- packages/utils/ts-utils/src/guard/index.ts
+++ packages/utils/ts-utils/src/guard/index.ts
@@ -4,3 +4,4 @@ export * from './is-non-null-object';
 export * from './is-primitive';
 export * from './is-record';
 export * from './is-type';
+export * from './key-is-in';
---
+++ packages/utils/ts-utils/src/guard/key-is-in.test.ts
@@ -0,0 +1,19 @@
+import { keyIsIn } from './key-is-in';
+
+const f = <Key extends string, V, KeySub extends Key>(
+  key: Key,
+  obj: Record<KeySub, V>
+): V | undefined => (keyIsIn(key, obj) ? obj[key] : undefined);
+
+describe('keyIsIn', () => {
+  f('a' as 'a' | 'b' | 'c', { a: 0, b: 1 });
+
+  test('', () => {
+    expect(
+      keyIsIn('a', {
+        a: 0,
+        b: 1,
+      })
+    ).toBe(true);
+  });
+});
