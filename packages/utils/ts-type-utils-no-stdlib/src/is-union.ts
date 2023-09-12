0334769aa83f1f4e95b7330b7253632a9e171cd5
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2023-09-12 10:37:13 +0900

    fix: ts-type-utils-no-stdlib: fix IsUnion implementation (#940)

--- packages/utils/ts-type-utils-no-stdlib/src/is-union.ts
+++ packages/utils/ts-type-utils-no-stdlib/src/is-union.ts
@@ -1,5 +1,6 @@
 import { type BoolNot } from './boolean';
 import { type IsNever } from './is-never';
+import { type TypeEq } from './utils';
 
 export type IsUnion<U> = _IsUnionImpl<U, U>;
 
@@ -7,5 +8,5 @@ export type IsUnion<U> = _IsUnionImpl<U, U>;
 type _IsUnionImpl<U, K extends U> = IsNever<U> extends true
   ? false
   : K extends K
-  ? BoolNot<IsNever<Exclude<U, K>>>
+  ? BoolNot<TypeEq<U, K>>
   : never;
