d90d5dd7c92c5faa7d4f4f66c2233f1b9a313b45
noshiro-pf <noshiro.pf@gmail.com>
2023-09-12 17:36:14 +0900

    feat: preserve space and newline in notes

--- packages/apps/event-schedule-app/src/components/organisms/answer-page-notes.tsx
+++ packages/apps/event-schedule-app/src/components/organisms/answer-page-notes.tsx
@@ -5,21 +5,23 @@ type Props = Readonly<{
   notes: string;
 }>;
 
-const convertUrlStringToAnchorElement = (
-  str: string,
-  key: React.Key
-): React.JSX.IntrinsicElements['a'] | React.JSX.IntrinsicElements['span'] =>
-  urlRegex({ exact: true, strict: false }).test(str) ? (
-    <a key={key} href={str} rel={'noopener noreferrer'} target={'_blank'}>
-      {str}
-    </a>
-  ) : (
-    <span key={key}>{str}</span>
-  );
-
 export const AnswerPageNotes = memoNamed<Props>(
   'AnswerPageNotes',
   ({ notes }) => (
     <>{splitStringByWhitespace(notes).map(convertUrlStringToAnchorElement)}</>
   )
 );
+
+const regex = urlRegex({ exact: true, strict: false });
+
+const convertUrlStringToAnchorElement = (
+  str: string,
+  key: React.Key
+): React.JSX.IntrinsicElements['a'] | React.JSX.IntrinsicElements['span'] =>
+  regex.test(str) ? (
+    <a key={key} href={str} rel={'noopener noreferrer'} target={'_blank'}>
+      {str}
+    </a>
+  ) : (
+    <span key={key}>{str}</span>
+  );
