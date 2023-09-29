222859a6301eccde05d86d9800e4ddccdd02c7c9
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2023-09-29 11:10:39 +0900

    feat: improve type annotation of getAlphabets (#974)

--- packages/utils/ts-utils-additional/src/string/get-alphabets.ts
+++ packages/utils/ts-utils-additional/src/string/get-alphabets.ts
@@ -3,11 +3,19 @@ import { type LowerAlphabet, type UpperAlphabet } from '../types';
 
 export const getAlphabets = <Case extends 'lower' | 'upper'>(
   charCase: Case
-): Case extends 'lower' ? LowerAlphabet[] : UpperAlphabet[] => {
+): [Case] extends ['lower']
+  ? LowerAlphabet[]
+  : [Case] extends ['upper']
+  ? UpperAlphabet[]
+  : never => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const code = charCase === 'lower' ? 'a'.codePointAt(0)! : 'A'.codePointAt(0)!;
 
-  return Arr.seq(26).map((i) =>
-    String.fromCodePoint(Uint32.add(code, i))
-  ) as Case extends 'lower' ? LowerAlphabet[] : UpperAlphabet[];
+  return Arr.seq(26).map((i) => String.fromCodePoint(Uint32.add(code, i))) as [
+    Case
+  ] extends ['lower']
+    ? LowerAlphabet[]
+    : [Case] extends ['upper']
+    ? UpperAlphabet[]
+    : never;
 };
