d1dc30fc71ee8ee455ecc9345d228ab65c428861
noshiro-pf <noshiro.pf@gmail.com>
2023-10-01 17:07:11 +0900

    feat: ts-utils-additional: improve type annotation of variance

--- packages/utils/ts-utils-additional/src/array/math/variance.ts
+++ packages/utils/ts-utils-additional/src/array/math/variance.ts
@@ -1,9 +1,17 @@
-import { Arr } from '@noshiro/ts-utils';
+import { Arr, toNonNegativeFiniteNumber } from '@noshiro/ts-utils';
 
 export const sqSum = (list: readonly number[]): number =>
   list.reduce((a, b) => a + b ** 2, 0);
 
-export const variance = (list: readonly number[]): number | undefined =>
-  list.length === 0
-    ? undefined
-    : sqSum(list) / list.length - Arr.sum(list) ** 2 / list.length ** 2;
+export function variance(list: NonEmptyArray<number>): NonNegativeFiniteNumber;
+export function variance(
+  list: readonly number[]
+): NonNegativeFiniteNumber | undefined;
+export function variance(
+  list: readonly number[]
+): NonNegativeFiniteNumber | undefined {
+  if (list.length === 0) return undefined;
+  return toNonNegativeFiniteNumber(
+    sqSum(list) / list.length - Arr.sum(list) ** 2 / list.length ** 2
+  );
+}
