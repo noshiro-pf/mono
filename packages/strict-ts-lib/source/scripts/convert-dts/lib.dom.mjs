a590fc718bc6314c6c0bee61996d8e671f4501e8
noshiro-pf <noshiro.pf@gmail.com>
2024-01-15 03:28:12 +0900

    feat: [strict-ts-lib] use enum values in HTTP Request method

--- packages/strict-ts-lib/source/scripts/convert-dts/lib.dom.mjs
+++ packages/strict-ts-lib/source/scripts/convert-dts/lib.dom.mjs
@@ -6,9 +6,45 @@ import { replaceWithNoMatchCheck } from '@noshiro/mono-scripts/ts-utils/replace-
  * @returns {string}
  */
 export const convertLibDom = (from) =>
-  pipe(from).chain(
-    replaceWithNoMatchCheck(
-      'json(): Promise<unknown>;',
-      'json(): Promise<JSONType>;',
-    ),
-  ).value;
+  pipe(from)
+    .chain(
+      replaceWithNoMatchCheck(
+        'json(): Promise<unknown>;',
+        'json(): Promise<JSONType>;',
+      ),
+    )
+    .chain((s) => {
+      const slice = s.slice(
+        s.indexOf('interface Request extends Body {'),
+        s.indexOf('declare var Request: {'),
+      );
+
+      return replaceWithNoMatchCheck(
+        slice,
+        pipe(slice).chain(
+          replaceWithNoMatchCheck(
+            'readonly method: string;',
+            'readonly method: HTTPRequestMethod;',
+          ),
+        ).value,
+      )(s);
+    })
+    .chain((s) => {
+      const slice = s.slice(
+        s.indexOf(
+          'interface XMLHttpRequest extends XMLHttpRequestEventTarget {',
+        ),
+        s.indexOf('declare var XMLHttpRequest: {'),
+      );
+
+      // https://developer.mozilla.org/ja/docs/Web/HTML/Element/form#method
+      return replaceWithNoMatchCheck(
+        slice,
+        pipe(slice).chain(
+          replaceWithNoMatchCheck(
+            'method: string',
+            "method: 'post' | 'get' | 'dialog'",
+          ),
+        ).value,
+      )(s);
+    }).value;
