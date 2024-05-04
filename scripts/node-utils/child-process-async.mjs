7ba637baed1384ef8dc9f2e03db33a8d937ca36a
noshiro-pf <noshiro.pf@gmail.com>
2024-05-04 11:39:12 +0900

    fix package.json of eslint-utils

--- scripts/node-utils/child-process-async.mjs
+++ scripts/node-utils/child-process-async.mjs
@@ -1,4 +1,4 @@
 import { exec } from 'node:child_process';
-import { promisify } from 'node:util';
+import * as util from 'node:util';
 
-export const execAsync = promisify(exec);
+export const execAsync = util.promisify(exec);
