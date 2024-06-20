426a4a312dd7fc1cadced6afc380c6e4d7b404ca
noshiro-pf <noshiro.pf@gmail.com>
2024-06-20 10:47:20 +0900

    fix: set ipv4first to DefaultResultOrder

--- packages/apps/algo-app/configs/vite.config.ts
+++ packages/apps/algo-app/configs/vite.config.ts
@@ -1,11 +1,14 @@
 import { toThisDir } from '@noshiro/mono-scripts';
 import preact from '@preact/preset-vite';
 import legacy from '@vitejs/plugin-legacy';
+import dns from 'node:dns';
 import * as nodePath from 'node:path';
 import { defineConfig } from 'vite';
 import { createInjectDef } from './inject-def';
 
 const thisDir: string = toThisDir(import.meta.url);
+
+dns.setDefaultResultOrder('ipv4first');
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
