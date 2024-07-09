b3c3f236981271910562ebf9e9a474663ac25536
noshiro-pf <noshiro.pf@gmail.com>
2024-07-09 14:52:41 +0900

    refactor: share constants in scripts/commands

---
+++ scripts/commands/constants.mjs
@@ -0,0 +1,14 @@
+export const utilsDirs = [
+  'packages/utils',
+  'packages/apps/lambda-calculus-interpreter-core',
+  'packages/apps/event-schedule-app-shared',
+];
+
+export const wsrunOptions = [
+  '--exclude-missing',
+  '--fast-exit',
+  '--prefix ',
+  '--serial',
+  '--ifDependency',
+  '--report',
+].join(' ');
--- scripts/commands/lint-apps.mjs
+++ scripts/commands/lint-apps.mjs
@@ -1,14 +1,8 @@
 import { forEachPackages } from '../esm/index.mjs';
+import { wsrunOptions } from './constants.mjs';
 
 forEachPackages({
   prefixes: ['packages/apps'],
   command: 'lint',
-  wsrunOptions: [
-    '--exclude-missing',
-    '--fast-exit',
-    '--prefix ',
-    '--serial',
-    '--ifDependency',
-    '--report',
-  ].join(' '),
+  wsrunOptions,
 }).catch(console.error);
