240026450cce6b0a9af68c382ba5f0497c6871d9
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2024-07-10 00:22:02 +0900

    ci: add type-check-eslint-configs command (#1232)

--- packages/tools/eslint-custom-rules/eslint.config.js
+++ packages/tools/eslint-custom-rules/eslint.config.js
@@ -1,37 +1,30 @@
 /** @typedef {import('@noshiro/eslint-utils').FlatConfig} FlatConfig */
 
+import { eslintFlatConfigCommon } from '@noshiro/eslint-utils';
 import { toThisDir } from '@noshiro/mono-scripts';
 import * as nodePath from 'node:path';
-import { eslintFlatConfigCommonWithRules } from '../../../config/eslint/flat-configs/common-with-rules.mjs';
 
 const thisDir = toThisDir(import.meta.url);
 
 /** @returns {Promise<readonly FlatConfig[]>} */
 const defineConfig = async () => {
-  /** @type {FlatConfig} */
-  const config = {
-    files: ['src/**/*.ts'],
-
-    languageOptions: {
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: thisDir,
+  /** @type {readonly FlatConfig[]} */
+  const configs = [
+    ...eslintFlatConfigCommon({
+      tsconfigRootDir: thisDir,
+      tsconfigFileName: './tsconfig.json',
+      packageDirs: [nodePath.resolve(thisDir, '../../..'), thisDir],
+    }),
+    {
+      files: ['src/**/*.ts'],
+      rules: {
+        'import/no-internal-modules': 'off',
+        'import/no-cycle': 'off',
       },
     },
-    rules: {
-      'import/no-extraneous-dependencies': [
-        'error',
-        {
-          packageDir: [nodePath.resolve(thisDir, '../../..'), thisDir],
-        },
-      ],
+  ];
 
-      'import/no-internal-modules': 'off',
-      'import/no-cycle': 'off',
-    },
-  };
-
-  return [...eslintFlatConfigCommonWithRules, config];
+  return configs;
 };
 
 export default defineConfig();
