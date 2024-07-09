a2b2642158a6533044cc63abfaa435f9dcaa13ab
noshiro-pf <noshiro.pf@gmail.com>
2024-07-09 09:28:55 +0900

    ci: add type-check-eslint-configs command

--- packages/others/ts_playground/eslint.config.js
+++ packages/others/ts_playground/eslint.config.js
@@ -1,29 +1,19 @@
 /** @typedef {import('@noshiro/eslint-utils').FlatConfig} FlatConfig */
 
-import {
-  eslintFlatConfigCommon,
-  genEsLintRestrictedImportsDefFromDevDependencies,
-} from '@noshiro/eslint-utils';
+import { eslintFlatConfigCommon } from '@noshiro/eslint-utils';
 import { toThisDir } from '@noshiro/mono-scripts';
 import * as nodePath from 'node:path';
-import packageJson from './package.json' assert { type: 'json' };
 
 const thisDir = toThisDir(import.meta.url);
 
 /** @returns {Promise<readonly FlatConfig[]>} */
 const defineConfig = async () => {
-  const restrictedImports =
-    await genEsLintRestrictedImportsDefFromDevDependencies(
-      packageJson.devDependencies,
-    );
-
   /** @type {readonly FlatConfig[]} */
   const configs = [
     ...eslintFlatConfigCommon({
       tsconfigRootDir: thisDir,
       tsconfigFileName: './tsconfig.json',
       packageDirs: [nodePath.resolve(thisDir, '../../..'), thisDir],
-      restrictedImports,
     }),
     {
       rules: {
