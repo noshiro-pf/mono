0db2bc4e04fbc2b9de522822e226c601fe603f78
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2024-08-28 14:19:43 +0900

    feat: create slack-archive-tools (#1286)

---
+++ packages/others/slack-archive-tools/configs/vitest.config.ts
@@ -0,0 +1,18 @@
+import { toThisDir } from '@noshiro/mono-scripts';
+import * as nodePath from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+const thisDir: string = toThisDir(import.meta.url);
+
+// https://github.com/vitest-dev/vitest/blob/v1.5.0/test/import-meta/vite.config.ts
+export default defineConfig({
+  test: {
+    globals: true,
+    dir: nodePath.resolve(thisDir, '../src'),
+    includeSource: [nodePath.resolve(thisDir, '../src/**/*.mts')],
+    typecheck: {
+      tsconfig: nodePath.resolve(thisDir, 'tsconfig.test.json'),
+    },
+    passWithNoTests: true,
+  },
+});
---
+++ packages/others/slack-archive-tools/src/main.mts
@@ -0,0 +1,5 @@
+export const main = (): void => {
+  console.log('TODO');
+};
+
+main();
