a5f8ab13c4377e1de31e560b00322113c056bd37
noshiro-pf <noshiro.pf@gmail.com>
2024-09-26 04:38:43 +0900

    .

--- packages/strict-ts-lib/output-branded/lib-files/lib.dom.d.ts
+++ packages/strict-ts-lib/output-branded/lib-files/lib.dom.d.ts
@@ -16510,7 +16510,8 @@ declare const HTMLLabelElement: {
 };
 
 /**
- * The HTMLLegendElement is an interface allowing to access properties of the<legend> elements. It inherits properties and methods from the HTMLElement
+ * The HTMLLegendElement is an interface allowing to access properties of
+ * the<legend> elements. It inherits properties and methods from the HTMLElement
  * interface.
  *
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLLegendElement)
