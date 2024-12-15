fe0676546aa202faecf689ec3a3b71f37eeff466
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2024-12-16 02:15:37 +0900

    fix: fix multiple exports in mahjong-calculator-app (#1360)

--- packages/apps/mahjong-calculator-app/src/constants/seats.ts
+++ packages/apps/mahjong-calculator-app/src/constants/seats.ts
@@ -3,4 +3,7 @@ export const Seats = {
   Kamicha: { id: 1, displayName: '上家' },
   Toimen: { id: 2, displayName: '対面' },
   Shimocha: { id: 3, displayName: '下家' },
-};
+} as const satisfies Record<
+  string,
+  Readonly<{ id: UintRange<0, 4>; displayName: string }>
+>;
