da6e984c99e174ff7390f2a4bd41795098b56b93
noshiro-pf <noshiro.pf@gmail.com>
2025-02-11 20:51:06 +0900

    create playground implement-react-hooks

---
+++ packages/others/implement-react-hooks/configs/vitest.config.ts
@@ -0,0 +1,18 @@
+import { toThisDir } from '@noshiro/mono-utils';
+import * as path from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+const thisDir: string = toThisDir(import.meta.url);
+
+// https://github.com/vitest-dev/vitest/blob/v1.5.0/test/import-meta/vite.config.ts
+export default defineConfig({
+  test: {
+    globals: true,
+    dir: path.resolve(thisDir, '../src'),
+    includeSource: [path.resolve(thisDir, '../src/**/*.mts')],
+    typecheck: {
+      tsconfig: path.resolve(thisDir, 'tsconfig.test.json'),
+    },
+    passWithNoTests: true,
+  },
+});
