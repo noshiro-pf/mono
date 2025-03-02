20d60cd87476a8b697f7407420243f8723e1ca97
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2025-03-02 15:16:02 +0900

    chore: add experimental/my-svelte-app (#1442)

---
+++ experimental/my-svelte-app/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Even from './even.svelte';
+  import Odd from './odd.svelte';
+
+  let count = $state(0);
+	let doubled = $derived(count * 2);
+
+	$effect(() => {
+		console.log({doubled})
+	});
+
+  const increment = () => {
+    count++;
+  };
+</script>
+
+<div>
+  <p>カウント: {count}</p>
+  <p>カウント x 2: {doubled}</p>
+	<div>{count % 2 === 0 ? "偶数" : "奇数"}</div>
+	<div>{#if count % 2 === 0}<Even/>{:else}<Odd />{/if}</div>
+  <button onclick="{increment}">カウントアップ</button>
+</div>
