47ba2cccf91bee370c8dcef968ee075e24e1df3d
noshiro-pf <noshiro.pf@gmail.com>
2025-03-19 17:01:17 +0900

    fix: fix index_ts_generator.sh

--- scripts/bash/index_ts_generator.sh
+++ scripts/bash/index_ts_generator.sh
@@ -95,7 +95,7 @@ for directory in $(find . -mindepth "${min_recursion_depth}" -maxdepth "${max_re
 
             # only files that match a regular expression "[a-zA-Z0-9_]+.ts"
             if [ "${filename}" != "index${ext}" ]; then
-                if [[ ${filename} =~ ${TS_FILENAME_REGEX} ]]; then
+                if [[ "${filename}" =~ ${TS_FILENAME_REGEX} ]] && ! echo "$filename" | grep -q '\.test\.'; then
                     # add line "export * from 'filename'"
                     if [ "${extension}" = "mts" ]; then
                         result+="export * from './${filename_without_ext}.mjs';"
