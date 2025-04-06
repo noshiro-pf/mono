d308654072a3f1df4a56b7c61b2c2d3216e36a34
noshiro-pf <noshiro.pf@gmail.com>
2025-04-07 05:32:43 +0900

    chore: update configs/define-rollup-config.mts

--- configs/define-rollup-config.mjs
+++ configs/define-rollup-config.mjs
@@ -2,29 +2,30 @@ import * as rollupPluginReplace from '@rollup/plugin-replace';
 import * as pluginTypescript from '@rollup/plugin-typescript';
 import * as glob from 'glob';
 import path from 'node:path';
-export const defineRollupConfig = ({ configDir, outDirRelative, }) => ({
-    input: glob.globSync(path.resolve(configDir, '../src/**/*.mts'), {
-        ignore: '**/*.test.mts',
+
+export const defineRollupConfig = ({ configDir, outDirRelative }) => ({
+  input: glob.globSync(path.resolve(configDir, '../src/**/*.mts'), {
+    ignore: '**/*.test.mts',
+  }),
+  output: {
+    format: 'es',
+    dir: path.resolve(configDir, outDirRelative),
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+    sourcemap: true,
+    entryFileNames: '[name].mjs',
+  },
+  plugins: [
+    rollupPluginReplace.default({
+      'import.meta.vitest': 'undefined',
+      preventAssignment: true,
     }),
-    output: {
-        format: 'es',
-        dir: path.resolve(configDir, outDirRelative),
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-        sourcemap: true,
-        entryFileNames: '[name].mjs',
-    },
-    plugins: [
-        rollupPluginReplace.default({
-            'import.meta.vitest': 'undefined',
-            preventAssignment: true,
-        }),
-        pluginTypescript.default({
-            tsconfig: path.resolve(configDir, './tsconfig.build.json'),
-        }),
-        rollupPluginReplace.default({
-            "import 'vitest'": 'undefined',
-            preventAssignment: true,
-        }),
-    ],
+    pluginTypescript.default({
+      tsconfig: path.resolve(configDir, './tsconfig.build.json'),
+    }),
+    rollupPluginReplace.default({
+      "import 'vitest'": 'undefined',
+      preventAssignment: true,
+    }),
+  ],
 });
