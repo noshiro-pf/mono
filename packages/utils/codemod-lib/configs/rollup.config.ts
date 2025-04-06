d7489364a4d1ceff57fe9a7740ae377c9f814d49
noshiro-pf <noshiro.pf@gmail.com>
2025-04-07 05:42:11 +0900

    codemod

--- packages/utils/codemod-lib/configs/rollup.config.ts
+++ packages/utils/codemod-lib/configs/rollup.config.ts
@@ -1,40 +1,12 @@
 /* eslint-disable import/no-default-export */
-/* eslint-disable import/no-internal-modules */
 
+// eslint-disable-next-line import/no-internal-modules
+import { defineRollupConfig } from '@noshiro/mono-configs/define-rollup-config';
 import { toThisDir } from '@noshiro/mono-utils';
-import * as rollupPluginReplace from '@rollup/plugin-replace';
-import * as pluginTypescript from '@rollup/plugin-typescript';
-import path from 'node:path';
-import { defineConfig } from 'rollup';
-import { defineRollupConfig } from '../../../../configs/define-rollup-config.mjs';
 import tsconfig from './tsconfig.build.json' with { type: 'json' };
 
-const configDir = toThisDir(import.meta.url);
-
-const defaultConfig = defineRollupConfig({
-  configDir,
+export default defineRollupConfig({
+  configDir: toThisDir(import.meta.url),
   outDirRelative: tsconfig.compilerOptions.outDir,
-});
-
-export default defineConfig({
-  ...defaultConfig,
-  plugins: [
-    // @ts-expect-error ???
-    rollupPluginReplace.default({
-      'import.meta.vitest': 'undefined',
-      'console.debug': 'undefined',
-      preventAssignment: true,
-    }),
-
-    // @ts-expect-error ???
-    pluginTypescript.default({
-      tsconfig: path.resolve(configDir, './tsconfig.build.json'),
-    }),
-
-    // @ts-expect-error ???
-    rollupPluginReplace.default({
-      "import 'vitest'": 'undefined',
-      preventAssignment: true,
-    }),
-  ],
+  variablesToDrop: ['console.debug'],
 });
