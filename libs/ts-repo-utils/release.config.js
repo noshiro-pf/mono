c26e1f254ed176a5828983f68bd55e309ba20e1c
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2025-06-23 04:49:12 +0900

    ci: update workflow settings (#18)

--- libs/ts-repo-utils/release.config.js
+++ libs/ts-repo-utils/release.config.js
@@ -10,15 +10,15 @@ export default {
     // 3. Write the release notes content generated in step 2 to the Changelog file.
     '@semantic-release/changelog',
 
+    // 4. Run prettier and build before committing.
+    ['@semantic-release/exec', { prepareCmd: 'npm run fmt && npm run build' }],
+
     // 4-a. Update the version field in package.json with the next version number.
     // 4-b. Publish the package to npmjs.
     // 4-c. Run `npm dist-tag` command to add a tag to the package published on npmjs.
     '@semantic-release/npm',
 
-    // 5-1. Run prettier to format the code before committing.
-    ['@semantic-release/exec', { prepareCmd: 'npm run fmt' }],
-
-    // 5-2. Commit the changes of assets generated during the release flow to the repository.
+    // 5. Commit the changes of assets generated during the release flow to the repository.
     [
       '@semantic-release/git',
       {
