973e47aeb5fe5cccca4d888d0e3dd8f0a3cec974
Hideaki Noshiro <46040697+noshiro-pf@users.noreply.github.com>
2025-10-15 06:49:18 +0900

    chore: fix vitest.config.ts (#31)

--- libs/octokit-safe-types/configs/vitest.config.ts
+++ libs/octokit-safe-types/configs/vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     dir: projectRootPath,
-    includeSource: [path.resolve(projectRootPath, './src/**/*.mts')],
+    includeSource: ['src/**/*.mts'],
     typecheck: {
       tsconfig: path.resolve(projectRootPath, './configs/tsconfig.test.json'),
     },
